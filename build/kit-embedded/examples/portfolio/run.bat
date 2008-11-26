53fd4dd90e7d4aa350d4f32d5ffbe448e3508cdb
Ramesh Reddy <rareddy@jboss.org>
2008-11-26 20:15:51 +0000

    FEDERATE-12: removing the unnecessary jars from embedded-kit, also separating the connector specific dependencies to "extension" directory

--- build/kit-embedded/examples/portfolio/run.bat
+++ build/kit-embedded/examples/portfolio/run.bat
@@ -2,7 +2,7 @@ rem First one sets the path for client jars and VDB
 set CLIENT_PATH=java/*;PortfolioModel/
 
 rem Second one for the JARs in federate embedded
-set FEDERATE_PATH=../../federate-0.0.1-SNAPSHOT-embedded.jar;../../lib/*
+set FEDERATE_PATH=../../federate-0.0.1-SNAPSHOT-embedded.jar;../../lib/*;../../extensions/*
 
 java -cp %CLIENT_PATH%;%FEDERATE_PATH% JDBCClient "select * from CustomerAccount"
 
--- build/kit-embedded/examples/portfolio/run.sh
+++ build/kit-embedded/examples/portfolio/run.sh
@@ -4,6 +4,6 @@
 CLIENT_PATH=java/*:PortfolioModel/
 
 #Second one for the JARs in federate embedded
-FEDERATE_PATH=../../federate-0.0.1-SNAPSHOT-embedded.jar:../../lib/*
+FEDERATE_PATH=../../federate-0.0.1-SNAPSHOT-embedded.jar:../../lib/*:../../extensions/*
 
 java -cp ${CLIENT_PATH}:${FEDERATE_PATH} JDBCClient "select * from CustomerAccount"
