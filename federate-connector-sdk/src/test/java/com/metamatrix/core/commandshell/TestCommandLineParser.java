2e7d6e1d2dadc72c6e9110006b0b534c22586328
Steve Hawkins <shawkins@redhat.com>
2009-01-06 16:59:15 +0000

    FEDERATE-162 moving command shell code to cdk

---
+++ federate-connector-sdk/src/test/java/com/metamatrix/core/commandshell/TestCommandLineParser.java
@@ -0,0 +1,65 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (C) 2008 Red Hat, Inc.
+ * Copyright (C) 2000-2007 MetaMatrix, Inc.
+ * Licensed to Red Hat, Inc. under one or more contributor 
+ * license agreements.  See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ */
+
+package com.metamatrix.core.commandshell;
+
+import junit.framework.TestCase;
+
+public class TestCommandLineParser extends TestCase {
+
+    public TestCommandLineParser(String name) {
+        super(name);
+    }
+
+    public void test() {
+        String command = "checkIn samplePath testdata/fakeFile \"Oct 1, 2003\""; //$NON-NLS-1$
+        String[] results = new CommandLineParser().parse(command);
+        assertEquals("checkIn", results[0]);     //$NON-NLS-1$
+        assertEquals("samplePath", results[1]);     //$NON-NLS-1$
+        assertEquals("testdata/fakeFile", results[2]);     //$NON-NLS-1$
+        assertEquals("Oct 1, 2003", results[3]);     //$NON-NLS-1$
+    }
+    
+    public void testSimple() {
+        String command = "getLatest samplePath"; //$NON-NLS-1$
+        String[] results = new CommandLineParser().parse(command);
+        assertEquals("getLatest", results[0]);     //$NON-NLS-1$
+        assertEquals("samplePath", results[1]);     //$NON-NLS-1$
+    }
+    
+    public void testTabsAsWhitespace() {
+        String command = "getLatest\tsamplePath"; //$NON-NLS-1$
+        String[] results = new CommandLineParser().parse(command);
+        assertEquals("getLatest", results[0]);     //$NON-NLS-1$
+        assertEquals("samplePath", results[1]);     //$NON-NLS-1$
+    }
+    
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+}
