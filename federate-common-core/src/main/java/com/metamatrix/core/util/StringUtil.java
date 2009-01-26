c2fd9f0486850b427412fcf626c32342dafc1f87
Steve Hawkins <shawkins@redhat.com>
2009-01-26 01:20:44 +0000

    correcting copyright and license issues

--- federate-common-core/src/main/java/com/metamatrix/core/util/StringUtil.java
+++ federate-common-core/src/main/java/com/metamatrix/core/util/StringUtil.java
@@ -2,24 +2,13 @@
  * JBoss, Home of Professional Open Source.
  * Copyright (C) 2008 Red Hat, Inc.
  * Copyright (C) 2000-2007 MetaMatrix, Inc.
- * Licensed to Red Hat, Inc. under one or more contributor 
+ * Licensed to Red Hat, Inc. under one or more contributor
  * license agreements.  See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * All rights reserved.
+ * This code is made available under the terms of the Eclipse Public
+ * License, version 1.0.
  */
 
 package com.metamatrix.core.util;
