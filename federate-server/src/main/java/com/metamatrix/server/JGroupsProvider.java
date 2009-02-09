f4deab0c426657e790b218f4a4d98ddaf655f8c0
Ramesh Reddy <rareddy@jboss.org>
2009-02-09 18:22:52 +0000

    FEDERATE-340: getting rid of unnecessary static state.

--- federate-server/src/main/java/com/metamatrix/server/JGroupsProvider.java
+++ federate-server/src/main/java/com/metamatrix/server/JGroupsProvider.java
@@ -41,6 +41,7 @@ import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import com.metamatrix.common.config.CurrentConfiguration;
 import com.metamatrix.common.config.ResourceNames;
+import com.metamatrix.common.config.api.exceptions.ConfigurationException;
 import com.metamatrix.common.log.LogManager;
 import com.metamatrix.common.util.LogCommonConstants;
 import com.metamatrix.common.util.VMNaming;
@@ -53,19 +54,17 @@ import com.metamatrix.core.util.StringUtil;
 @Singleton
 public class JGroupsProvider implements Provider<org.jgroups.mux.Multiplexer> {
 
-	private static String properties;
-
 	// Default Multicast Property values
-	private static final String DEFAULT_UDP_MCAST_SUPPORTED  = "true"; //$NON-NLS-1$
-	private static final String DEFAULT_UDP_MCAST_ADDR_PREFIX  = "224.224.223."; //$NON-NLS-1$
-	private static final String DEFAULT_UDP_MCAST_PORT           = "5555"; //$NON-NLS-1$
-    private static final String ALL_INTERFACES_ADDR = "0.0.0.0";  //$NON-NLS-1$
+	private static final String DEFAULT_UDP_MCAST_SUPPORTED = "true"; //$NON-NLS-1$
+	private static final String DEFAULT_UDP_MCAST_ADDR_PREFIX = "224.224.223."; //$NON-NLS-1$
+	private static final String DEFAULT_UDP_MCAST_PORT = "5555"; //$NON-NLS-1$
+	private static final String ALL_INTERFACES_ADDR = "0.0.0.0"; //$NON-NLS-1$
 
 	// Default Gossip Server Property values
-	private static final String DEFAULT_PING_GOSSIP_HOST         = "localhost"; //$NON-NLS-1$
-	private static final String DEFAULT_PING_GOSSIP_PORT         = "5555"; //$NON-NLS-1$
-	private static final String DEFAULT_PING_GOSSIP_REFRESH      = "15000"; //$NON-NLS-1$
-	private static final String DEFAULT_PING_TIMEOUT             = "2000"; //$NON-NLS-1$
+	private static final String DEFAULT_PING_GOSSIP_HOST = "localhost"; //$NON-NLS-1$
+	private static final String DEFAULT_PING_GOSSIP_PORT = "5555"; //$NON-NLS-1$
+	private static final String DEFAULT_PING_GOSSIP_REFRESH = "15000"; //$NON-NLS-1$
+	private static final String DEFAULT_PING_TIMEOUT = "2000"; //$NON-NLS-1$
 	private static final String DEFAULT_PING_NUM_INITIAL_MEMBERS = "3"; //$NON-NLS-1$
     
     private static final String DEFAULT_JGROUPS_OTHER_SETTINGS =            
@@ -81,37 +80,15 @@ public class JGroupsProvider implements Provider<org.jgroups.mux.Multiplexer> {
                     "pbcast.STATE_TRANSFER"; //$NON-NLS-1$
                 
     private static final String UDP_MCAST_SUPPORTED_PROPERTY = "udp.multicast_supported"; //$NON-NLS-1$
-    private static final String UDP_MCAST_MESSAGEBUS_PORT_PROPERTY = "udp.mcast_messagebus_port";     //$NON-NLS-1$
-    private static final String UDP_MCAST_JNDI_PORT_PROPERTY       = "udp.mcast_jndi_port";     //$NON-NLS-1$
-    private static final String UDP_MCAST_ADDR_PROPERTY      = "udp.mcast_addr"; //$NON-NLS-1$
-    private static final String PING_GOSSIP_HOST_PROPERTY    = "ping.gossip_host"; //$NON-NLS-1$
-    private static final String PING_GOSSIP_PORT_PROPERTY    = "ping.gossip_port"; //$NON-NLS-1$
-    private static final String PING_GOSSIP_REFRESH_PROPERTY      = "ping.gossip_refresh"; //$NON-NLS-1$
-    private static final String PING_GOSSIP_TIMEOUT_PROPERTY             = "ping.gossip_timout"; //$NON-NLS-1$
-    private static final String PING_GOSSIP_NUM_INITIAL_MEMBERS_PROPERTY = "ping.gossip_initialmembers"; //$NON-NLS-1$
-    private static final String JGROUPS_OTHER_SETTINGS_PROPERTY = "jgroups.other.channel.settings"; //$NON-NLS-1$
-    private static final String BIND_ADDRESS_PROPERTY = "jgroups.bind.address"; //$NON-NLS-1$
-    
-    private static final String MESSAGE_BUS_CHANNEL = "MessageBus"; //$NON-NLS-1$
-    
-    // Multicast Properties
-    private static String UDP_MCAST_SUPPORTED      = DEFAULT_UDP_MCAST_SUPPORTED;
-    private static String UDP_MCAST_ADDR           = null; 
-        //DEFAULT_UDP_MCAST_ADDR;
-    private static String UDP_MCAST_PORT           = DEFAULT_UDP_MCAST_PORT;
-    
-    private static Boolean UDP_RECV_ON_ALL_INTERFACES = Boolean.FALSE;
-
-    // Gossip Server Properties
-    private static String PING_GOSSIP_HOST         = DEFAULT_PING_GOSSIP_HOST;
-    private static String PING_GOSSIP_PORT         = DEFAULT_PING_GOSSIP_PORT;
-    private static String PING_GOSSIP_REFRESH      = DEFAULT_PING_GOSSIP_REFRESH;
-    private static String PING_TIMEOUT             = DEFAULT_PING_TIMEOUT;
-    private static String PING_NUM_INITIAL_MEMBERS = DEFAULT_PING_NUM_INITIAL_MEMBERS;
-    
-    private static String JGROUPS_OTHER_SETTINGS = DEFAULT_JGROUPS_OTHER_SETTINGS;
-    
-    private static String BIND_ADDRESS = null;
+	private static final String UDP_MCAST_MESSAGEBUS_PORT_PROPERTY = "udp.mcast_messagebus_port"; //$NON-NLS-1$
+	private static final String UDP_MCAST_ADDR_PROPERTY = "udp.mcast_addr"; //$NON-NLS-1$
+	private static final String PING_GOSSIP_HOST_PROPERTY = "ping.gossip_host"; //$NON-NLS-1$
+	private static final String PING_GOSSIP_PORT_PROPERTY = "ping.gossip_port"; //$NON-NLS-1$
+	private static final String PING_GOSSIP_REFRESH_PROPERTY = "ping.gossip_refresh"; //$NON-NLS-1$
+	private static final String PING_GOSSIP_TIMEOUT_PROPERTY = "ping.gossip_timout"; //$NON-NLS-1$
+	private static final String PING_GOSSIP_NUM_INITIAL_MEMBERS_PROPERTY = "ping.gossip_initialmembers"; //$NON-NLS-1$
+	private static final String JGROUPS_OTHER_SETTINGS_PROPERTY = "jgroups.other.channel.settings"; //$NON-NLS-1$
+	private static final String BIND_ADDRESS_PROPERTY = "jgroups.bind.address"; //$NON-NLS-1$
     
     /**
 	 * Helper method to create a JChannel.
@@ -123,13 +100,14 @@ public class JGroupsProvider implements Provider<org.jgroups.mux.Multiplexer> {
     
     public Multiplexer get() {
 		try {
-			JChannel channel=new JChannel(getChannelProperties(MESSAGE_BUS_CHANNEL));
+			JChannel channel=new JChannel(getChannelProperties());
 			Multiplexer mux = new Multiplexer(channel);
 			
+			// JMX needs to moved out here, such that it can be registered and unregistered when shutdown occurs.
 			// register the channel with the JMX server
 			try {
 				MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-				ObjectName on = new ObjectName("Federate:service=JChannel,name="+MESSAGE_BUS_CHANNEL); //$NON-NLS-1$
+				ObjectName on = new ObjectName("Federate:service=JChannel,name=JGroups"); //$NON-NLS-1$
 				mbs.registerMBean(new org.jgroups.jmx.JChannel(channel), on);
 			} catch (MalformedObjectNameException e) {
 				LogManager.logWarning(LogCommonConstants.CTX_CONFIG, "Failed to register JChannel to JMX"); //$NON-NLS-1$
@@ -145,76 +123,66 @@ public class JGroupsProvider implements Provider<org.jgroups.mux.Multiplexer> {
 		} 
 	}
 
-	private static synchronized String getChannelProperties(String name) {
-        // Based on the channel name "name" get the property name for the appropriate channel port.
-        String port = null;
-        if (MESSAGE_BUS_CHANNEL.equals(name)) {
-            port = UDP_MCAST_MESSAGEBUS_PORT_PROPERTY;
-        } else {
-            port = UDP_MCAST_JNDI_PORT_PROPERTY;
-        }
-        
-	    try {
-	        Properties configProps = CurrentConfiguration.getInstance().getResourceProperties(ResourceNames.JGROUPS);
+	private synchronized String getChannelProperties() {
 
-            UDP_MCAST_SUPPORTED      =  configProps.getProperty(UDP_MCAST_SUPPORTED_PROPERTY, DEFAULT_UDP_MCAST_SUPPORTED);
+		try {
+        	String properties = null;
+			Properties configProps = CurrentConfiguration.getInstance().getResourceProperties(ResourceNames.JGROUPS);
 
-	        UDP_MCAST_PORT           = configProps.getProperty(port, DEFAULT_UDP_MCAST_PORT);
-	        PING_GOSSIP_HOST         = configProps.getProperty(PING_GOSSIP_HOST_PROPERTY, DEFAULT_PING_GOSSIP_HOST);
-	        PING_GOSSIP_PORT         = configProps.getProperty(PING_GOSSIP_PORT_PROPERTY, DEFAULT_PING_GOSSIP_PORT);
-	        PING_GOSSIP_REFRESH      = configProps.getProperty(PING_GOSSIP_REFRESH_PROPERTY, DEFAULT_PING_GOSSIP_REFRESH);
-	        PING_TIMEOUT             = configProps.getProperty(PING_GOSSIP_TIMEOUT_PROPERTY, DEFAULT_PING_TIMEOUT);
-	        PING_NUM_INITIAL_MEMBERS = configProps.getProperty(PING_GOSSIP_NUM_INITIAL_MEMBERS_PROPERTY, DEFAULT_PING_NUM_INITIAL_MEMBERS);
-            JGROUPS_OTHER_SETTINGS = configProps.getProperty(JGROUPS_OTHER_SETTINGS_PROPERTY, DEFAULT_JGROUPS_OTHER_SETTINGS);
+			String udpMulticastSupported =  configProps.getProperty(UDP_MCAST_SUPPORTED_PROPERTY, DEFAULT_UDP_MCAST_SUPPORTED);
 
-            BIND_ADDRESS = getBindAddress();
-            
-            
-            if (UDP_MCAST_ADDR == null) {
-                UDP_MCAST_ADDR           = configProps.getProperty(UDP_MCAST_ADDR_PROPERTY);
-                if (UDP_MCAST_ADDR == null || UDP_MCAST_ADDR.length() == 0) {
-                    // use the last node of the local machine address as the last node
-                    // of the DEFAULT address.
-                    String lastNode = StringUtil.getLastToken(VMNaming.getBindAddress(), ".");//$NON-NLS-1$
-                    
-                    UDP_MCAST_ADDR = DEFAULT_UDP_MCAST_ADDR_PREFIX + lastNode;
-                }
-            }            
-            
-        } catch (Exception e) {
-            // If there is an error reading currentConfig then use default properties. 
-        }
-        
-		if (UDP_MCAST_SUPPORTED.equalsIgnoreCase("true")) { //$NON-NLS-1$
-            
-            if (UDP_RECV_ON_ALL_INTERFACES.booleanValue()) {
-                properties = 
-                    "UDP(mcast_addr="+UDP_MCAST_ADDR+";mcast_port="+UDP_MCAST_PORT+";ip_ttl=32;receive_on_all_interfaces=" + UDP_MCAST_SUPPORTED.toString() + ";" + //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-                    "mcast_send_buf_size=150000;mcast_recv_buf_size=80000):" + //$NON-NLS-1$
-                    "PING(timeout="+PING_TIMEOUT+";num_initial_members="+PING_NUM_INITIAL_MEMBERS+"):";//$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
-                
-                
-            } else {
-    			properties = 
-    				"UDP(mcast_addr="+UDP_MCAST_ADDR+";mcast_port="+UDP_MCAST_PORT+";ip_ttl=32;bind_addr="+BIND_ADDRESS+";"+   //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-    				"mcast_send_buf_size=150000;mcast_recv_buf_size=80000):" + //$NON-NLS-1$
-    				"PING(timeout="+PING_TIMEOUT+";num_initial_members="+PING_NUM_INITIAL_MEMBERS+"):";//$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
-            }
-                //timeout=2000;num_initial_members=3):"; //$NON-NLS-1$
-		} else {
-			properties = 
-				"UDP(ip_mcast=false;mcast_addr="+UDP_MCAST_ADDR+";mcast_port="+UDP_MCAST_PORT+";bind_addr="+BIND_ADDRESS+"):" + //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-				"PING(gossip_host="+PING_GOSSIP_HOST+";gossip_port="+PING_GOSSIP_PORT+";gossip_refresh="+PING_GOSSIP_REFRESH+";" + //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-				"timeout="+PING_TIMEOUT+";num_initial_members="+PING_NUM_INITIAL_MEMBERS+"):";  //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
-		}
-		properties += JGROUPS_OTHER_SETTINGS;
+			String udpMulticastPort = configProps.getProperty(UDP_MCAST_MESSAGEBUS_PORT_PROPERTY, DEFAULT_UDP_MCAST_PORT);
+			String pingGossipHost         = configProps.getProperty(PING_GOSSIP_HOST_PROPERTY, DEFAULT_PING_GOSSIP_HOST);
+			String pingGossipPort         = configProps.getProperty(PING_GOSSIP_PORT_PROPERTY, DEFAULT_PING_GOSSIP_PORT);
+			String pingGossipRerefresh      = configProps.getProperty(PING_GOSSIP_REFRESH_PROPERTY, DEFAULT_PING_GOSSIP_REFRESH);
+			String pingTimeout             = configProps.getProperty(PING_GOSSIP_TIMEOUT_PROPERTY, DEFAULT_PING_TIMEOUT);
+			String pingInitialMemberCount = configProps.getProperty(PING_GOSSIP_NUM_INITIAL_MEMBERS_PROPERTY, DEFAULT_PING_NUM_INITIAL_MEMBERS);
+			String otherSettings = configProps.getProperty(JGROUPS_OTHER_SETTINGS_PROPERTY, DEFAULT_JGROUPS_OTHER_SETTINGS);
 
-		return properties;
+			String bindAddress = getBindAddress();
+			boolean multicastOnAllInterfaces = bindAddress.equalsIgnoreCase(ALL_INTERFACES_ADDR);
+
+		    String udpMulticastAddress = configProps.getProperty(UDP_MCAST_ADDR_PROPERTY);
+		    if (udpMulticastAddress == null || udpMulticastAddress.length() == 0) {
+		        // use the last node of the local machine address as the last node
+		        // of the DEFAULT address.
+		        String lastNode = StringUtil.getLastToken(bindAddress, ".");//$NON-NLS-1$
+		        
+		        udpMulticastAddress = DEFAULT_UDP_MCAST_ADDR_PREFIX + lastNode;
+		    }
+
+		
+			if (udpMulticastSupported.equalsIgnoreCase("true")) { //$NON-NLS-1$
+	            if (multicastOnAllInterfaces) {
+	                properties = 
+	                    "UDP(mcast_addr="+udpMulticastAddress+";mcast_port="+udpMulticastPort+";ip_ttl=32;receive_on_all_interfaces=" + udpMulticastSupported.toString() + ";" + //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	                    "mcast_send_buf_size=150000;mcast_recv_buf_size=80000):" + //$NON-NLS-1$
+	                    "PING(timeout="+pingTimeout+";num_initial_members="+pingInitialMemberCount+"):";//$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+	                
+	                
+	            } else {
+	    			properties = 
+	    				"UDP(mcast_addr="+udpMulticastAddress+";mcast_port="+udpMulticastPort+";ip_ttl=32;bind_addr="+bindAddress+";"+   //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	    				"mcast_send_buf_size=150000;mcast_recv_buf_size=80000):" + //$NON-NLS-1$
+	    				"PING(timeout="+pingTimeout+";num_initial_members="+pingInitialMemberCount+"):";//$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+	            }
+			} else {
+				properties = 
+					"UDP(ip_mcast=false;mcast_addr="+udpMulticastAddress+";mcast_port="+udpMulticastPort+";bind_addr="+bindAddress+"):" + //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+					"PING(gossip_host="+pingGossipHost+";gossip_port="+pingGossipPort+";gossip_refresh="+pingGossipRerefresh+";" + //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+					"timeout="+pingTimeout+";num_initial_members="+pingInitialMemberCount+"):";  //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+			}
+			properties += otherSettings;
+			return properties;
+			
+		} catch (ConfigurationException e) {
+			throw new MetaMatrixRuntimeException(e);
+		}            
 	}
     
-    private static final String getBindAddress() {
-        String bindAddress = System.getProperty(JGroupsProvider.BIND_ADDRESS_PROPERTY);
+    private final String getBindAddress() {
         // check for command line system property being set from vm.starter.command for jgroup
+        String bindAddress = System.getProperty(JGroupsProvider.BIND_ADDRESS_PROPERTY);
         if (bindAddress == null)  { 
             bindAddress = VMNaming.getBindAddress();
         }
@@ -223,12 +191,6 @@ public class JGroupsProvider implements Provider<org.jgroups.mux.Multiplexer> {
             LogManager.logWarning(LogCommonConstants.CTX_MESSAGE_BUS,"WARNING: Unable to set " + JGroupsProvider.BIND_ADDRESS_PROPERTY + ", will set to 127.0.0.1"); //$NON-NLS-1$ //$NON-NLS-2$
             bindAddress = "127.0.0.1";//$NON-NLS-1$
         }
-        
-        if (bindAddress.equalsIgnoreCase(ALL_INTERFACES_ADDR)) {
-            UDP_RECV_ON_ALL_INTERFACES = Boolean.TRUE;
-        }
-
         return bindAddress;
-
     }
 }
