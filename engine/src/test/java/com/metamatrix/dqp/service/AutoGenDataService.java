981c724a2fa53064f344fb3027397cd3ece05341
Van Halbert <vhalbert@redhat.com>
2009-05-18 18:43:16 +0000

    Teiid-580 - adding support for monitoring connector connection pools - exposing the stats in embedded

--- engine/src/test/java/com/metamatrix/dqp/service/AutoGenDataService.java
+++ engine/src/test/java/com/metamatrix/dqp/service/AutoGenDataService.java
@@ -29,17 +29,14 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-import org.teiid.dqp.internal.datamgr.impl.ConnectorManager;
 import org.teiid.dqp.internal.datamgr.impl.ConnectorWorkItem;
 import org.teiid.dqp.internal.process.DQPWorkContext;
 
-import com.metamatrix.api.exception.ComponentNotFoundException;
 import com.metamatrix.api.exception.MetaMatrixComponentException;
 import com.metamatrix.common.application.exception.ApplicationLifecycleException;
 import com.metamatrix.common.comm.api.ResultsReceiver;
 import com.metamatrix.common.config.api.ConnectorBinding;
 import com.metamatrix.common.types.DataTypeManager;
-import com.metamatrix.dqp.embedded.DQPEmbeddedPlugin;
 import com.metamatrix.dqp.internal.datamgr.ConnectorID;
 import com.metamatrix.dqp.message.AtomicRequestID;
 import com.metamatrix.dqp.message.AtomicRequestMessage;
