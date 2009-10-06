5b61bb4575030037b838e60cde49f2d01531ec0e
Van Halbert <vhalbert@redhat.com>
2009-10-06 15:13:18 +0000

    Teiid 773 -  refactored out of the test code into the main framework code

---
+++ test-integration/db/src/main/java/org/teiid/test/framework/query/QueryExecution.java
@@ -0,0 +1,21 @@
+package org.teiid.test.framework.query;
+
+import java.sql.Connection;
+
+import com.metamatrix.jdbc.api.AbstractQueryTest;
+
+/**
+ * The QueryExecution class can be used to query the source directly.   The intended use
+ * of this class is for validating results after the execution of a test case.
+ * 
+ * @see AbstractQueryTransactionTest regarding creating a testcase to validate behavior.
+ * @author vanhalbert
+ *
+ */
+public class QueryExecution extends AbstractQueryTest {
+	
+	public QueryExecution(Connection conn) {
+		super(conn);
+	}
+
+}
