612b1e770cd80f6a96691290766cfd941e2c9ada
Van Halbert <vhalbert@redhat.com>
2010-02-15 16:13:34 +0000

    Teiid-773 - changed to support running the testcases in hudson

--- test-integration/db/runclienttest.sh
+++ test-integration/db/runclienttest.sh
@@ -11,7 +11,17 @@ QS_DIR=./src/main/resources/ctc_tests/queries
 
 VDB_DIR=./src/main/resources/transactions
 
-mvn integration-test  -Dscenario.dir=$SC_DIR -Dqueryset.artifacts.dir=$QS_DIR -Dvdb.artifacts.dir=$VDB_DIR -Prunclienttests
+
+
+if [ -z "${datasourceloc}" ] 
+	then
+	
+	datasourceloc='./src/main/resources/datasources'
+fi
+
+echo "Use Datasources: ${DS1} and ${DS2} loc: ${datasourceloc}"
+
+mvn integration-test -Dusedatasources=${DS1},${DS2}  -Ddatasourceloc=${datasourceloc}  -Dscenario.dir=$SC_DIR -Dqueryset.artifacts.dir=$QS_DIR -Dvdb.artifacts.dir=$VDB_DIR -Prunclienttests
 
 
 
