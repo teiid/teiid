d5801f174fdef34369a53f7f65fb6347db05b57d
Van Halbert <vhalbert@redhat.com>
2010-04-09 19:29:27 +0000

    Teiid-773 -  changes package the ctc kit, needed to add the version specific ant dependencies, otherwise it was pulling ant 1.6 (instead of 1.7)

--- test-integration/db/runclienttest.sh
+++ test-integration/db/runclienttest.sh
@@ -1,27 +1,168 @@
 #!/bin/sh
 
-#  This script will run the client tests based on the
-#  below project scenarios.  
+#  This script will run the client tests 
 
-#  The scenario
+#============================
+#	REQUIRED PROPERTIES
+#
+#	-	USEDATASOURCES   -	indicates the datasource(s) (common separated) to use, this is the directory located inside directory datasourceloc 
+$
 
-SC_DIR=./src/main/resources/ctc_tests/scenarios
-QS_DIR=./src/main/resources/ctc_tests/queries
-# VDB_DIR=./src/main/resources/ctc_tests/vdbs
+#============================
+#	OPTIONAL PROPERTIES
+#
+#  (if not set, will default to files within the db project)
+#	-	QUERYSETDIR	-	directory location where the test querties can be found]
+#	-	SCENARIODIR	-	directory location where the scenario files found and determine which query sets and vdbs to use, 
+#	-	DATASOURCEDIR	-	root directory location to find the various datasources to use
+#	-	CONFIGFILE	-	specify the configuration file to use (override ctc-test.properties)
 
-VDB_DIR=./src/main/resources/transactions
+######################################################
+#	DEBUGGING OPTION
+#
+#    JBEDSP_DEBUG - uncomment to enable the output of debug messages.
+#                 
+# JBEDSP_DEBUG=true
+#
 
 
-
-if [ -z "${datasourceloc}" ] 
+# the property that defines which datasources to use.  these names must exist as directories within the datasource loc.
+if [ -z "$USEDATASOURCES" ] 
 	then
 	
-	datasourceloc='./src/main/resources/datasources'
+		echo ""
+		echo "Property <USEDATASOURCES> must be specified"
+		echo "Exit"
+		exit
 fi
 
-echo "Use Datasources: ${DS1} and ${DS2} loc: ${datasourceloc}"
 
-mvn integration-test -Dusedatasources=${DS1},${DS2}  -Ddatasourceloc=${datasourceloc}  -Dscenario.dir=$SC_DIR -Dqueryset.artifacts.dir=$QS_DIR -Dvdb.artifacts.dir=$VDB_DIR -Prunclienttests
+
+#if [ -z "${vdb.artifacts.dir}" ] 
+#	then
+#	
+#	vdb.artifacts.dir='./src/main/resources/transactions'
+#	
+#fi
+
+if [ -z "${QUERYSETDIR}" ] 
+	then
+	
+	QUERYSETDIR='./src/main/resources/ctc_tests/queries'
+	
+fi
+
+if [ -z "${SCENARIODIR}" ] 
+	then
+	
+	SCENARIODIR='./src/main/resources/ctc_tests/scenarios'
+	
+fi
+
+
+if [ -z "${DATASOURCEDIR}" ] 
+	then
+	
+	DATASOURCEDIR='./src/main/resources/datasources'
+fi
+
+echo "Use Datasources: ${USEDATASOURCES} loc: ${DATASOURCEDIR}"
+
+#--------------------
+
+# resolve links - $0 may be a softlink
+LOC="$0"
+
+while [ -h "$LOC" ] ; do
+  ls=`ls -ld "$LOC"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    LOC="$link"
+  else
+    LOC=`dirname "$LOC"`/"$link"
+  fi
+done
+
+LOCDIR=`dirname "$LOC"`
+PRGDIR=`cd "$LOCDIR"; pwd`
+
+cd "${PRGDIR}"
+
+echo "============================"
+echo "Running tests from ${PRGDIR}"
+echo "============================"
+
+# check if running from the kit
+if [  -f "${PRGDIR}/resources/ctc_tests/ctc.xml" ]; then
+   CTCXML=${PRGDIR}/resources/ctc_tests/ctc.xml
+else
+	CTCXML='./src/main/resources/ctc_tests/ctc.xml'
+ 
+fi
+
+
+ANT_ARGS=" -Dusedatasources=${USEDATASOURCES} "
+ANT_ARGS="${ANT_ARGS} -Ddatasourceloc=${DATASOURCEDIR}"
+ANT_ARGS="${ANT_ARGS} -Dscenario.dir=${SCENARIODIR}"
+ANT_ARGS="${ANT_ARGS} -Dqueryset.artifacts.dir=${QUERYSETDIR}"
+#ANT_ARGS="${ANT_ARGS} -Dvdb.artifacts.dir=${vdb.artifacts.dir}"
+ANT_ARGS="${ANT_ARGS} -Dproj.dir=${PRGDIR}"
+
+
+
+if [ ! -z "${XMLCLZZ}" ] 
+	then
+	
+	ANT_ARGS="${ANT_ARGS} -Dquery.scenario.classname=$XMLCLZZ"
+	
+fi
+
+if [ ! -z "${CONFIGFILE}" ] 
+	then
+	
+	ANT_ARGS="${ANT_ARGS} -Dconfig.file=${CONFIGFILE}"
+	
+fi
+
+if [ ! -z "${PROPFILE}" ] 
+	then
+	
+	ANT_ARGS=" -propertyfile $PROPFILE $ANT_ARGS "
+	
+fi
+
+
+ANT_OPTS="-Xmx512m"
+ANT_HOME=${PRGDIR}/ant
+
+# uncomment for additional debugging info in the logfile
+# turn on debug for additional debugging info in the logfile
+if [ -n "$JBEDSP_DEBUG" ]; then
+	ANT_ARGS="${ANT_ARGS} -verbose"
+ fi
+ 
+if [ ! -x "${PRGDIR}/log" ]; then
+    echo "Create ${PRGDIR}/log directory"
+	mkdir "${PRGDIR}"/log
+fi
+
+if [ -n "${JAVA_OPTS}" ]; then
+	ANT_OPTS="${JAVA_OPTS} $ANT_OPTS "
+fi
+
+CP="${PRGDIR}:${PRGDIR}/ant/*"
+
+LOGLEVEL=info
+
+echo "ANT BUILDFILE=${CTCXML}"
+echo "ANT_HOME=${ANT_HOME}"
+echo "ANT_ARGS=${ANT_ARGS}"
+echo "CP=$CP"
+
+java -cp "${CP}" -Dant.home="${ANT_HOME}" $ANT_OPTS org.apache.tools.ant.Main $ANT_ARGS -buildfile ${CTCXML}
+
+
+
 
 
 
