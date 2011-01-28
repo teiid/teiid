ea81af7a84cfb3a27161011999dc83b89fad78b2
Steve Hawkins <shawkins@redhat.com>
2011-01-28 16:30:06 +0000

    TEIID-1451 ensuring that the logon result gets cleared when disconnected

--- runtime/src/test/java/org/teiid/transport/TestFailover.java
+++ runtime/src/test/java/org/teiid/transport/TestFailover.java
@@ -28,6 +28,7 @@ import static org.mockito.Mockito.*;
 import java.net.InetSocketAddress;
 import java.util.Properties;
 
+import org.junit.After;
 import org.junit.Test;
 import org.teiid.client.security.ILogon;
 import org.teiid.client.security.InvalidSessionException;
@@ -58,6 +59,15 @@ public class TestFailover {
 	private SocketServerConnectionFactory sscf;
 	private InetSocketAddress addr = new InetSocketAddress(0);
 	private int logonAttempts;
+	
+	@After public void tearDown() {
+		if (this.listener != null) {
+			this.listener.stop();
+		}
+		if (this.listener1 != null) {
+			this.listener1.stop();
+		}
+	}
 
 	private SocketServerConnection helpEstablishConnection(boolean clientSecure, SSLConfiguration config, Properties socketConfig) throws CommunicationException,
 			ConnectionException {
