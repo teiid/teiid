fbc3e1f2640e5da8d9b8906742ff0d5996574e86
Steve Hawkins <shawkins@redhat.com>
2011-08-18 20:41:00 +0000

    forward merge of 7.4.1 fixes

--- runtime/src/main/java/org/teiid/transport/PgBackendProtocol.java
+++ runtime/src/main/java/org/teiid/transport/PgBackendProtocol.java
@@ -97,6 +97,7 @@ public class PgBackendProtocol implements ChannelDownstreamHandler, ODBCClientRe
 		private int rows2Send;
 		private int rowsSent = 0;
 		private int rowsInBuffer = 0;
+		String sql;
 
 		private ResultsWorkItem(List<PgColInfo> cols, ResultSetImpl rs, ResultsFuture<Integer> result, int rows2Send) {
 			this.cols = cols;
@@ -150,6 +151,9 @@ public class PgBackendProtocol implements ChannelDownstreamHandler, ODBCClientRe
     				}
     			} else {
     				sendContents();
+    				if (sql != null) {
+		    			sendCommandComplete(sql, 0);
+		    		}
     				result.getResultsReceiver().receiveResults(rowsSent);
     				processNext = false;
     			}
@@ -355,8 +359,8 @@ public class PgBackendProtocol implements ChannelDownstreamHandler, ODBCClientRe
     		sendRowDescription(cols);
     	}
     	ResultsWorkItem r = new ResultsWorkItem(cols, rs, result, -1);
+    	r.sql = sql;
     	r.run();    
-    	sendCommandComplete(sql, 0);
 	}
 
 	@Override
@@ -602,6 +606,12 @@ public class PgBackendProtocol implements ChannelDownstreamHandler, ODBCClientRe
 	}
 	
 	private void sendRowDescription(List<PgColInfo> cols) {
+		if (cols == null) {
+			//send NoData
+			startMessage('n');
+			sendMessage();
+			return;
+		}
 		startMessage('T');
 		writeShort(cols.size());
 		for (PgColInfo info : cols) {
@@ -785,6 +795,7 @@ public class PgBackendProtocol implements ChannelDownstreamHandler, ODBCClientRe
 	private void sendContents() {
 		ChannelBuffer cb = this.dataOut;
 		this.dataOut = null;
+		this.writer = null;
 		Channels.write(this.ctx, this.message.getFuture(), cb, this.message.getRemoteAddress());
 	}
 
