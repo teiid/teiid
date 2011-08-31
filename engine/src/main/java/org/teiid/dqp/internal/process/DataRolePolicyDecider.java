80ccb9bc0953f1793e1f5bce71299116b272771e
Steve Hawkins <shawkins@redhat.com>
2011-08-31 18:43:49 +0000

    TEIID-1738 applying temptable permissions appropriately

--- engine/src/main/java/org/teiid/dqp/internal/process/DataRolePolicyDecider.java
+++ engine/src/main/java/org/teiid/dqp/internal/process/DataRolePolicyDecider.java
@@ -1,3 +1,25 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ */
+
 package org.teiid.dqp.internal.process;
 
 import java.util.Collections;
@@ -49,13 +71,20 @@ public class DataRolePolicyDecider implements PolicyDecider {
 	@Override
 	public boolean isTempAccessable(PermissionType action, String resource,
 			Context context, CommandContext commandContext) {
+		Boolean result = null;
     	for(DataPolicy p:commandContext.getAllowedDataPolicies().values()) {
 			DataPolicyMetadata policy = (DataPolicyMetadata)p;
 			
 			if (policy.isAllowCreateTemporaryTables() != null) {
-				return policy.isAllowCreateTemporaryTables();
+				if (policy.isAllowCreateTemporaryTables()) {
+					return true;
+				}
+				result = policy.isAllowCreateTemporaryTables();
 			}
 		}
+    	if (result != null) {
+    		return result;
+    	}
     	return allowCreateTemporaryTablesByDefault;
 	}
 	
