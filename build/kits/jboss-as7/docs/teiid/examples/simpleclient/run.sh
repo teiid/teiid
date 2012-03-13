c97c5165b57ee9ca94e3048caa8a9030a2e7d858
Ramesh Reddy <rareddy@jboss.org>
2012-03-13 19:55:06 +0000

    TEIID-1839: replacing weather with twitter web service example.

--- build/kits/jboss-as7/docs/teiid/examples/simpleclient/run.sh
+++ build/kits/jboss-as7/docs/teiid/examples/simpleclient/run.sh
@@ -4,6 +4,6 @@
 CLIENT_PATH=.
 
 #Second one for the Teiid client jar
-TEIID_PATH=../../../modules/org/jboss/teiid/common-core/main/teiid-common-core-${pom.version}.jar:../../../modules/org/jboss/teiid/client/main/teiid-client-${pom.version}.jar
+TEIID_PATH=../../../../modules/org/jboss/teiid/common-core/main/teiid-common-core-${pom.version}.jar:../../../../modules/org/jboss/teiid/client/main/teiid-client-${pom.version}.jar
 
 java -cp ${CLIENT_PATH}:${TEIID_PATH} JDBCClient "$@"
