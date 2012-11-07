cc272922c03fdba3489e27b212d3c2ad74df3d42
Van Halbert <vhalbert@redhat.com>
2012-11-07 10:49:02 -0600

    TEIID-2299 remove the examples that were converted to quick starts and update release notes

--- build/kits/jboss-as7/docs/teiid/examples/simpleclient/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# First one sets the path for the client
-CLIENT_PATH=.
-
-#Second one for the Teiid client jar
-TEIID_PATH=../../../../modules/org/jboss/teiid/common-core/main/teiid-common-core-${pom.version}.jar:../../../../modules/org/jboss/teiid/client/main/teiid-client-${pom.version}.jar
-
-java -cp ${CLIENT_PATH}:${TEIID_PATH} JDBCClient "$@"
