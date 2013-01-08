5496c33fc61cad81cedb5bda695224f69fc1d410
Ramesh Reddy <rareddy@jboss.org>
2013-01-08 08:35:38 -0600

    Update odata/src/test/java/org/teiid/odata/TestODataIntegration.java
    need to make this arquallian integration test

--- odata/src/test/java/org/teiid/odata/TestODataIntegration.java
+++ odata/src/test/java/org/teiid/odata/TestODataIntegration.java
@@ -41,6 +41,7 @@ import org.jboss.resteasy.test.EmbeddedContainer;
 import org.jboss.resteasy.test.TestPortProvider;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.odata4j.core.*;
@@ -53,6 +54,7 @@ import org.teiid.core.util.UnitTestUtil;
 import org.teiid.query.metadata.TransformationMetadata;
 import org.teiid.query.unittest.RealMetadataFactory;
 
+@Ignore
 public class TestODataIntegration extends BaseResourceTest {
 	
 	@BeforeClass
