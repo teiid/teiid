0cffbfeb3dbee37040884d36990631319eccffb2
Ramesh Reddy <rareddy@jboss.org>
2013-04-08 14:01:52 -0500

    Adding a way pass ws-security credentials through ws connector

--- api/src/main/java/org/teiid/resource/spi/TeiidSecurityCredential.java
+++ api/src/main/java/org/teiid/resource/spi/TeiidSecurityCredential.java
@@ -3,17 +3,17 @@
  * See the COPYRIGHT.txt file distributed with this work for information
  * regarding copyright ownership.  Some portions may be licensed
  * to Red Hat, Inc. under one or more contributor license agreements.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -29,28 +29,32 @@ import javax.resource.spi.ManagedConnectionFactory;
 public class TeiidSecurityCredential implements Serializable {
 	private static final long serialVersionUID = 7725328159246858176L;
 
+	public enum SecurityHandler {WSPOLICY, WSS4J};
+
 	private HashMap<String, Object> requestProps = new HashMap<String, Object>();
-	private HashMap<String, Object> responseProps = new HashMap<String, Object>(); 
-	
+	private HashMap<String, Object> responseProps = new HashMap<String, Object>();
+
+
 	private ManagedConnectionFactory mcf;
+	private SecurityHandler securityHandler = SecurityHandler.WSS4J;
 
 	/**
 	 * Gets the target ManagedConnectionFactory for which the user name and
 	 * password has been set by the application server. A ManagedConnection-
 	 * Factory uses this field to find out whether PasswordCredential should be
 	 * used by it for sign-on to the target EIS instance.
-	 * 
+	 *
 	 * @return ManagedConnectionFactory instance for which user name and
 	 *         password have been specified
 	 **/
 	public ManagedConnectionFactory getManagedConnectionFactory() {
-		return mcf;
+		return this.mcf;
 	}
 
 	/**
 	 * Sets the target ManagedConenctionFactory instance for which the user name
 	 * and password has been set by the application server.
-	 * 
+	 *
 	 * @param mcf
 	 *            ManagedConnectionFactory instance for which user name and
 	 *            password have been specified
@@ -58,12 +62,20 @@ public class TeiidSecurityCredential implements Serializable {
 	public void setManagedConnectionFactory(ManagedConnectionFactory mcf) {
 		this.mcf = mcf;
 	}
-	
+
 	public HashMap<String, Object> getRequestPropterties(){
-		return requestProps;
+		return this.requestProps;
 	}
-	
+
 	public HashMap<String, Object> getResponsePropterties(){
-		return responseProps;
-	}	
+		return this.responseProps;
+	}
+
+	public SecurityHandler getSecurityHandler() {
+		return this.securityHandler;
+	}
+
+	public void setSecurityHandler(SecurityHandler securityHandler) {
+		this.securityHandler = securityHandler;
+	}
 }
