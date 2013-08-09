17f402bb4c729627cef17c662ea79379cb59b46d
shawkins <shawkins@redhat.com>
2013-08-09 14:22:44 -0400

    TEIID-2619 ensuring source closure

--- engine/src/main/java/org/teiid/dqp/internal/process/DataTierManagerImpl.java
+++ engine/src/main/java/org/teiid/dqp/internal/process/DataTierManagerImpl.java
@@ -167,6 +167,7 @@ public class DataTierManagerImpl implements ProcessorDataManager {
 		@Override
 		public void onCompletion(FutureWork<Void> future) {
 			signalMore();
+			toRead.closeSource();
 			dtts.fullyCloseSource();
 		}
 	}
