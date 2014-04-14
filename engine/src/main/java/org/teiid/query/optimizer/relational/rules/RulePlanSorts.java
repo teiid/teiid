6aa473ddb6ee2ef49eb49f7190f85aa1d7d554e3
shawkins <shawkins@redhat.com>
2014-04-14 11:04:12 -0400

    TEIID-2921 allowing more optimization with complex sorts

--- engine/src/main/java/org/teiid/query/optimizer/relational/rules/RulePlanSorts.java
+++ engine/src/main/java/org/teiid/query/optimizer/relational/rules/RulePlanSorts.java
@@ -23,6 +23,7 @@
 package org.teiid.query.optimizer.relational.rules;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import org.teiid.api.exception.query.QueryMetadataException;
@@ -36,6 +37,7 @@ import org.teiid.query.optimizer.relational.RuleStack;
 import org.teiid.query.optimizer.relational.plantree.NodeConstants;
 import org.teiid.query.optimizer.relational.plantree.NodeConstants.Info;
 import org.teiid.query.optimizer.relational.plantree.NodeEditor;
+import org.teiid.query.optimizer.relational.plantree.NodeFactory;
 import org.teiid.query.optimizer.relational.plantree.PlanNode;
 import org.teiid.query.processor.relational.JoinNode.JoinStrategyType;
 import org.teiid.query.processor.relational.MergeJoinStrategy.SortOption;
@@ -236,18 +238,34 @@ public class RulePlanSorts implements OptimizerRule {
 		List<Expression> childOutputCols = (List<Expression>) projectNode.getFirstChild().getProperty(Info.OUTPUT_COLS);
 		OrderBy orderBy = (OrderBy) node.getProperty(Info.SORT_ORDER);
 		List<Expression> orderByKeys = orderBy.getSortKeys();
+		LinkedHashSet<Expression> toProject = new LinkedHashSet();
 		for (Expression ss : orderByKeys) {
+			Expression original = ss;
 			if(ss instanceof AliasSymbol) {
                 ss = ((AliasSymbol)ss).getSymbol();
             }
             if (ss instanceof ExpressionSymbol) {
-                return root; //TODO: insert a new project node to handle this case
+            	if (!raiseAccess) {
+            		return root; //TODO: insert a new project node to handle this case
+            	}
             }
 			if (!childOutputCols.contains(ss)) {
-				return root;
+				if (!raiseAccess) {
+					return root;
+				}
+				toProject.add(original);
 			}
 		}
 		PlanNode toRepair = projectNode.getParent();
+		if (!toProject.isEmpty()) {
+			PlanNode intermediateProject = NodeFactory.getNewNode(NodeConstants.Types.PROJECT);
+			toProject.addAll(childOutputCols);
+			List<Expression> projectCols = new ArrayList<Expression>(toProject);
+			childOutputCols = projectCols;
+			intermediateProject.setProperty(NodeConstants.Info.PROJECT_COLS, projectCols);
+			intermediateProject.setProperty(NodeConstants.Info.OUTPUT_COLS, new ArrayList<Expression>(projectCols));
+			toRepair.getFirstChild().addAsParent(intermediateProject);
+		}
 		NodeEditor.removeChildNode(projectNode.getParent(), projectNode);
 		if (parent != null && parent.getType() == NodeConstants.Types.TUPLE_LIMIT && parent.getParent() != null) {
 			parent.addAsParent(projectNode);
@@ -271,14 +289,16 @@ public class RulePlanSorts implements OptimizerRule {
 			unrelated = true;
 		}
 		for (OrderByItem item : orderBy.getOrderByItems()) {
-			if (unrelated) {
+			if (unrelated || !toProject.isEmpty()) {
 			    //update sort order
 				int index = childOutputCols.indexOf(item.getSymbol());
 				item.setExpressionPosition(index);
 			}
-			//strip alias as project was raised
-			if (item.getSymbol() instanceof AliasSymbol) {
-				item.setSymbol(((AliasSymbol)item.getSymbol()).getSymbol());
+			if (toProject.isEmpty()) {
+				//strip alias as project was raised
+				if (item.getSymbol() instanceof AliasSymbol) {
+					item.setSymbol(((AliasSymbol)item.getSymbol()).getSymbol());
+				}
 			}
 		}
 		while (toRepair != node) {
@@ -292,17 +312,25 @@ public class RulePlanSorts implements OptimizerRule {
 			parent.setProperty(Info.OUTPUT_COLS, childOutputCols);
 		}
 		if (raiseAccess) {
-			PlanNode accessNode = node.getFirstChild();
+			PlanNode accessNode = NodeEditor.findNodePreOrder(node, NodeConstants.Types.ACCESS);
+			
 			//instead of just calling ruleraiseaccess, we're more selective
 			//we do not want to raise the access node over a project that is handling an unrelated sort
 			PlanNode newRoot = RuleRaiseAccess.raiseAccessNode(root, accessNode, metadata, capFinder, true, record, context);
 			if (newRoot != null) {
+				accessNode.setProperty(NodeConstants.Info.OUTPUT_COLS, childOutputCols);
 				root = newRoot;
-				if (accessNode.getParent().getType() == NodeConstants.Types.TUPLE_LIMIT) {
-					newRoot = RulePushLimit.raiseAccessOverLimit(root, accessNode, metadata, capFinder, accessNode.getParent(), record);
+				if (!toProject.isEmpty()) {
+					newRoot = RuleRaiseAccess.raiseAccessNode(root, accessNode, metadata, capFinder, true, record, context);
 				}
 				if (newRoot != null) {
 					root = newRoot;
+					if (accessNode.getParent().getType() == NodeConstants.Types.TUPLE_LIMIT) {
+						newRoot = RulePushLimit.raiseAccessOverLimit(root, accessNode, metadata, capFinder, accessNode.getParent(), record);
+					}
+					if (newRoot != null) {
+						root = newRoot;
+					}
 				}
 			}
 		}
