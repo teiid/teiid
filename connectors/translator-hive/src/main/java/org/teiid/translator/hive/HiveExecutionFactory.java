2257f4f8d9b9e5897cfa6616ce7c1cb5226c215b
Ramesh Reddy <rareddy@jboss.org>
2014-04-29 11:13:11 -0500

    TEIID-2647: adding a translator for cloudera's impala data source

--- connectors/translator-hive/src/main/java/org/teiid/translator/hive/HiveExecutionFactory.java
+++ connectors/translator-hive/src/main/java/org/teiid/translator/hive/HiveExecutionFactory.java
@@ -23,275 +23,107 @@ package org.teiid.translator.hive;
 
 import static org.teiid.translator.TypeFacility.RUNTIME_NAMES.*;
 
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.teiid.language.Command;
 import org.teiid.language.Function;
-import org.teiid.language.Limit;
-import org.teiid.metadata.AggregateAttributes;
-import org.teiid.metadata.FunctionMethod;
-import org.teiid.translator.*;
-import org.teiid.translator.jdbc.*;
+import org.teiid.translator.SourceSystemFunctions;
+import org.teiid.translator.Translator;
+import org.teiid.translator.TranslatorException;
+import org.teiid.translator.TypeFacility;
+import org.teiid.translator.jdbc.AliasModifier;
+import org.teiid.translator.jdbc.ConvertModifier;
+import org.teiid.translator.jdbc.FunctionModifier;
+import org.teiid.translator.jdbc.ModFunctionModifier;
 
 @Translator(name="hive", description="A translator for hive based database on HDFS")
-public class HiveExecutionFactory extends JDBCExecutionFactory {
+public class HiveExecutionFactory extends BaseHiveExecutionFactory {
 
-	public static String HIVE = "hive"; //$NON-NLS-1$
-	protected ConvertModifier convert = new ConvertModifier();
-
-	public HiveExecutionFactory() {
-		setSupportedJoinCriteria(SupportedJoinCriteria.EQUI);
-	}
-
-	@Override
-	public void start() throws TranslatorException {
-		super.start();
-		this.convert.addTypeMapping("tinyint", FunctionModifier.BYTE); //$NON-NLS-1$
-		this.convert.addTypeMapping("smallint", FunctionModifier.SHORT); //$NON-NLS-1$
-		this.convert.addTypeMapping("int", FunctionModifier.INTEGER); //$NON-NLS-1$
-		this.convert.addTypeMapping("bigint", FunctionModifier.BIGINTEGER, FunctionModifier.LONG); //$NON-NLS-1$
-		this.convert.addTypeMapping("boolean", FunctionModifier.BOOLEAN); //$NON-NLS-1$
-		this.convert.addTypeMapping("double", FunctionModifier.DOUBLE); //$NON-NLS-1$
-		this.convert.addTypeMapping("decimal", FunctionModifier.BIGDECIMAL); //$NON-NLS-1$
-		this.convert.addTypeMapping("float", FunctionModifier.FLOAT); //$NON-NLS-1$
-		this.convert.addTypeMapping("string", FunctionModifier.STRING); //$NON-NLS-1$
-		this.convert.addTypeMapping("timestamp", FunctionModifier.TIMESTAMP); //$NON-NLS-1$
-		this.convert.addTypeMapping("binary", FunctionModifier.BLOB); //$NON-NLS-1$
-		this.convert.addTypeMapping("decimal", FunctionModifier.BIGDECIMAL); //$NON-NLS-1$
-		this.convert.addTypeMapping("date", FunctionModifier.DATE); //$NON-NLS-1$
-
-		// unsupported types
-		//FunctionModifier.TIME,
-		//FunctionModifier.CHAR,
-		//FunctionModifier.CLOB,
-		//FunctionModifier.XML
-
-		registerFunctionModifier(SourceSystemFunctions.CONVERT, this.convert);
-
-		registerFunctionModifier(SourceSystemFunctions.BITAND, new AliasModifier("&")); //$NON-NLS-1$
-		registerFunctionModifier(SourceSystemFunctions.BITNOT, new AliasModifier("~")); //$NON-NLS-1$
-		registerFunctionModifier(SourceSystemFunctions.BITOR, new AliasModifier("|")); //$NON-NLS-1$
-		registerFunctionModifier(SourceSystemFunctions.BITXOR, new AliasModifier("^")); //$NON-NLS-1$
-		registerFunctionModifier(SourceSystemFunctions.CURDATE, new AliasModifier("unix_timestamp")); //$NON-NLS-1$
-		registerFunctionModifier(SourceSystemFunctions.IFNULL, new AliasModifier("coalesce")); //$NON-NLS-1$
-		registerFunctionModifier(SourceSystemFunctions.MOD, new ModFunctionModifier("%", getLanguageFactory(), Arrays.asList(TypeFacility.RUNTIME_TYPES.BIG_INTEGER, TypeFacility.RUNTIME_TYPES.BIG_DECIMAL))); //$NON-NLS-1$
-        registerFunctionModifier(SourceSystemFunctions.ARRAY_GET, new FunctionModifier() {
-
-			@Override
-			public List<?> translate(Function function) {
-				return Arrays.asList(function.getParameters().get(0), '[', function.getParameters().get(1), ']');
-			}
-		});
-
-		addPushDownFunction(HIVE, "lower", STRING, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "upper", STRING, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "positive", INTEGER, DOUBLE); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "positive", DOUBLE, DOUBLE); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "negitive", INTEGER, DOUBLE); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "negitive", DOUBLE, DOUBLE); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "ln", DOUBLE, DOUBLE); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "reverse", STRING, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "space", INTEGER, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "split", OBJECT, STRING, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "hex", STRING, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "unhex", STRING, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "bin", STRING, BIG_INTEGER); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "day", INTEGER, DATE); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "datediff", INTEGER, STRING, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "date_add", INTEGER, STRING, INTEGER); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "date_sub", INTEGER, STRING, INTEGER); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "from_unixtime", STRING, BIG_INTEGER); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "from_unixtime", STRING, BIG_INTEGER, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "unix_timestamp", BIG_INTEGER, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "unix_timestamp", BIG_INTEGER, STRING, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "to_date", STRING, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "from_utc_timestamp", TIMESTAMP, TIMESTAMP, STRING); //$NON-NLS-1$
-		addPushDownFunction(HIVE, "to_utc_timestamp", TIMESTAMP, TIMESTAMP, STRING); //$NON-NLS-1$
-		
-		addAggregatePushDownFunction(HIVE, "LEAD", OBJECT, OBJECT); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "LEAD", OBJECT, OBJECT, INTEGER); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "LEAD", OBJECT, OBJECT, INTEGER, OBJECT); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "LAG", OBJECT, OBJECT); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "LAG", OBJECT, OBJECT, INTEGER); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "LAG", OBJECT, OBJECT, INTEGER, OBJECT); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "FIRST_VALUE", OBJECT, OBJECT); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "LAST_VALUE", OBJECT, OBJECT); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "PERCENT_RANK", FLOAT); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "CUME_DIST", FLOAT); //$NON-NLS-1$
-		addAggregatePushDownFunction(HIVE, "NTILE", BIG_INTEGER, INTEGER); //$NON-NLS-1$
-		
-	}
-
-	@Override
-    public SQLConversionVisitor getSQLConversionVisitor() {
-    	return new HiveSQLConversionVisitor(this);
-    }
-
-	@Override
-    public boolean useAnsiJoin() {
-    	return true;
-    }
-
-    @Override
-    public boolean supportsCorrelatedSubqueries() {
-    	//https://issues.apache.org/jira/browse/HIVE-784
-        return false;
-    }
-
-    @Override
-    public boolean supportsExistsCriteria() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsInCriteriaSubquery() {
-    	// the website documents a way to semi-join to re-write this but did not handle NOT IN case.
-        return false;
-    }
-
-    @Override
-    public boolean supportsLikeCriteriaEscapeCharacter() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsQuantifiedCompareCriteriaAll() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsQuantifiedCompareCriteriaSome() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsBulkUpdate() {
-    	return false;
-    }
-
-    @Override
-    public boolean supportsBatchedUpdates() {
-    	return false;
-    }
-
-    @Override
-    public List<?> translateCommand(Command command, ExecutionContext context) {
-    	return null;
-    }
-
-    @Override
-    public List<?> translateLimit(Limit limit, ExecutionContext context) {
-    	return null;
-    }
-
-    @Override
-    public boolean addSourceComment() {
-        return false;
-    }
-
-    @Override
-    public boolean useAsInGroupAlias(){
-        return false;
-    }
-
-    @Override
-	public boolean hasTimeType() {
-    	return false;
-    }
-
-	@Override
-	public String getLikeRegexString() {
-		return "REGEXP"; //$NON-NLS-1$
-	}
-
-    @Override
-    public boolean supportsScalarSubqueries() {
-    	// Supported only in FROM clause
-        return false;
-    }
-
-    @Override
-    public boolean supportsInlineViews() {
-    	// must be aliased.
-        return true;
-    }
-
-    @Override
-    public boolean supportsUnions() {
-        return true;
-        // only union all in subquery
-    }
-
-    @Override
-    public boolean supportsInsertWithQueryExpression() {
-    	return false; // insert seems to be only with overwrite always
-    }
-
-    @Override
-    public boolean supportsIntersect() {
-    	return false;
-    }
-
-    @Override
-    public boolean supportsExcept() {
-    	return false;
-    }
-
-    @Override
-    public boolean supportsAggregatesEnhancedNumeric() {
-    	return true;
-    }
-
-    @Override
-    public boolean supportsCommonTableExpressions() {
-    	return false;
+    public static String HIVE = "hive"; //$NON-NLS-1$
+    
+    public HiveExecutionFactory() {
+        setSupportedJoinCriteria(SupportedJoinCriteria.EQUI);
     }
     
     @Override
-	public boolean supportsElementaryOlapOperations() {
-    	return true;
-    }
+    public void start() throws TranslatorException {
+        super.start();
 
-	@Override
-	public boolean supportsRowLimit() {
-		return true;
-	}
-	
-    @Override
-	public boolean supportsGroupByRollup() {
-    	//https://cwiki.apache.org/confluence/display/Hive/Enhanced+Aggregation,+Cube,+Grouping+and+Rollup
-		return true;
-	}
+        ConvertModifier convert = new ConvertModifier();
+        convert.addTypeMapping("tinyint", FunctionModifier.BYTE); //$NON-NLS-1$
+        convert.addTypeMapping("smallint", FunctionModifier.SHORT); //$NON-NLS-1$
+        convert.addTypeMapping("int", FunctionModifier.INTEGER); //$NON-NLS-1$
+        convert.addTypeMapping("bigint", FunctionModifier.BIGINTEGER, FunctionModifier.LONG); //$NON-NLS-1$
+        convert.addTypeMapping("boolean", FunctionModifier.BOOLEAN); //$NON-NLS-1$
+        convert.addTypeMapping("double", FunctionModifier.DOUBLE); //$NON-NLS-1$
+        convert.addTypeMapping("float", FunctionModifier.FLOAT); //$NON-NLS-1$
+        convert.addTypeMapping("string", FunctionModifier.STRING); //$NON-NLS-1$
+        convert.addTypeMapping("timestamp", FunctionModifier.TIMESTAMP); //$NON-NLS-1$
+        convert.addTypeMapping("binary", FunctionModifier.BLOB); //$NON-NLS-1$
+        convert.addTypeMapping("decimal", FunctionModifier.BIGDECIMAL); //$NON-NLS-1$
+        convert.addTypeMapping("date", FunctionModifier.DATE); //$NON-NLS-1$        
+        // unsupported types
+        //FunctionModifier.TIME,
+        //FunctionModifier.CHAR,
+        //FunctionModifier.CLOB,
+        //FunctionModifier.XML
 
-    @Override
-    public String translateLiteralBoolean(Boolean booleanValue) {
-        if(booleanValue.booleanValue()) {
-            return "true"; //$NON-NLS-1$
-        }
-        return "false"; //$NON-NLS-1$
-    }
+        registerFunctionModifier(SourceSystemFunctions.CONVERT, convert);
 
-    @Override
-    public String translateLiteralDate(java.sql.Date dateValue) {
-        return '\'' + formatDateValue(dateValue) + '\'';
-    }
+        registerFunctionModifier(SourceSystemFunctions.BITAND, new AliasModifier("&")); //$NON-NLS-1$
+        registerFunctionModifier(SourceSystemFunctions.BITNOT, new AliasModifier("~")); //$NON-NLS-1$
+        registerFunctionModifier(SourceSystemFunctions.BITOR, new AliasModifier("|")); //$NON-NLS-1$
+        registerFunctionModifier(SourceSystemFunctions.BITXOR, new AliasModifier("^")); //$NON-NLS-1$
+        registerFunctionModifier(SourceSystemFunctions.CURDATE, new AliasModifier("unix_timestamp")); //$NON-NLS-1$
+        registerFunctionModifier(SourceSystemFunctions.IFNULL, new AliasModifier("coalesce")); //$NON-NLS-1$
+        registerFunctionModifier(SourceSystemFunctions.MOD, new ModFunctionModifier("%", getLanguageFactory(), Arrays.asList(TypeFacility.RUNTIME_TYPES.BIG_INTEGER, TypeFacility.RUNTIME_TYPES.BIG_DECIMAL))); //$NON-NLS-1$
+        registerFunctionModifier(SourceSystemFunctions.ARRAY_GET, new FunctionModifier() {
 
-    @Override
-    public String translateLiteralTime(Time timeValue) {
-    	if (!hasTimeType()) {
-    		return translateLiteralTimestamp(new Timestamp(timeValue.getTime()));
-    	}
-    	return '\'' + formatDateValue(timeValue) + '\'';
-    }
+            @Override
+            public List<?> translate(Function function) {
+                return Arrays.asList(function.getParameters().get(0), '[', function.getParameters().get(1), ']');
+            }
+        });
 
-    @Override
-    public String translateLiteralTimestamp(Timestamp timestampValue) {
-        return '\'' + formatDateValue(timestampValue) + '\'';
-    }
-
+        addPushDownFunction(HIVE, "lower", STRING, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "upper", STRING, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "positive", INTEGER, DOUBLE); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "positive", DOUBLE, DOUBLE); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "negitive", INTEGER, DOUBLE); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "negitive", DOUBLE, DOUBLE); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "ln", DOUBLE, DOUBLE); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "reverse", STRING, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "space", INTEGER, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "split", OBJECT, STRING, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "hex", STRING, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "unhex", STRING, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "bin", STRING, BIG_INTEGER); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "day", INTEGER, DATE); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "datediff", INTEGER, STRING, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "date_add", INTEGER, STRING, INTEGER); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "date_sub", INTEGER, STRING, INTEGER); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "from_unixtime", STRING, BIG_INTEGER); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "from_unixtime", STRING, BIG_INTEGER, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "unix_timestamp", BIG_INTEGER, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "unix_timestamp", BIG_INTEGER, STRING, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "to_date", STRING, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "from_utc_timestamp", TIMESTAMP, TIMESTAMP, STRING); //$NON-NLS-1$
+        addPushDownFunction(HIVE, "to_utc_timestamp", TIMESTAMP, TIMESTAMP, STRING); //$NON-NLS-1$
+        
+        addAggregatePushDownFunction(HIVE, "LEAD", OBJECT, OBJECT); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "LEAD", OBJECT, OBJECT, INTEGER); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "LEAD", OBJECT, OBJECT, INTEGER, OBJECT); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "LAG", OBJECT, OBJECT); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "LAG", OBJECT, OBJECT, INTEGER); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "LAG", OBJECT, OBJECT, INTEGER, OBJECT); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "FIRST_VALUE", OBJECT, OBJECT); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "LAST_VALUE", OBJECT, OBJECT); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "PERCENT_RANK", FLOAT); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "CUME_DIST", FLOAT); //$NON-NLS-1$
+        addAggregatePushDownFunction(HIVE, "NTILE", BIG_INTEGER, INTEGER); //$NON-NLS-1$
+    }    
+    
     @Override
     public List<String> getSupportedFunctions() {
         List<String> supportedFunctions = new ArrayList<String>();
@@ -312,10 +144,10 @@ public class HiveExecutionFactory extends JDBCExecutionFactory {
         supportedFunctions.add(SourceSystemFunctions.CONCAT);
         supportedFunctions.add(SourceSystemFunctions.COS);
         supportedFunctions.add(SourceSystemFunctions.CONVERT);
-		supportedFunctions.add(SourceSystemFunctions.CURDATE);
-		supportedFunctions.add(SourceSystemFunctions.CURTIME);
-		supportedFunctions.add(SourceSystemFunctions.DEGREES);
-		supportedFunctions.add(SourceSystemFunctions.DAYOFMONTH);
+        supportedFunctions.add(SourceSystemFunctions.CURDATE);
+        supportedFunctions.add(SourceSystemFunctions.CURTIME);
+        supportedFunctions.add(SourceSystemFunctions.DEGREES);
+        supportedFunctions.add(SourceSystemFunctions.DAYOFMONTH);
         supportedFunctions.add(SourceSystemFunctions.EXP);
         supportedFunctions.add(SourceSystemFunctions.FLOOR);
         supportedFunctions.add(SourceSystemFunctions.HOUR);
@@ -345,38 +177,21 @@ public class HiveExecutionFactory extends JDBCExecutionFactory {
         supportedFunctions.add(SourceSystemFunctions.UCASE);
         supportedFunctions.add(SourceSystemFunctions.YEAR);
         return supportedFunctions;
-    }
-
-    @Deprecated
+    }    
+    
     @Override
-    protected JDBCMetdataProcessor createMetadataProcessor() {
-        return (HiveMetadataProcessor)getMetadataProcessor();
+    public boolean supportsAggregatesEnhancedNumeric() {
+        return true;
     }
     
     @Override
-    public MetadataProcessor<Connection> getMetadataProcessor(){
-        return new HiveMetadataProcessor();
-    }
-
-    @Override
-    public Object retrieveValue(ResultSet results, int columnIndex, Class<?> expectedType) throws SQLException {
-    	if (expectedType.equals(Timestamp.class)) {
-    		// Calendar based getTimestamp not supported by Hive
-    		return results.getTimestamp(columnIndex);
-    	}
-    	return super.retrieveValue(results, columnIndex, expectedType);
-    }
-    
-    protected FunctionMethod addAggregatePushDownFunction(String qualifier, String name, String returnType, String...paramTypes) {
-    	FunctionMethod method = addPushDownFunction(qualifier, name, returnType, paramTypes);
-    	AggregateAttributes attr = new AggregateAttributes();
-    	attr.setAnalytic(true);
-    	method.setAggregateAttributes(attr);
-    	return method;
-    }
+    public boolean supportsElementaryOlapOperations() {
+        return true;
+    }    
     
     @Override
-    public boolean supportsHaving() {
-    	return false; //only having with group by
-    }
+    public boolean supportsGroupByRollup() {
+        //https://cwiki.apache.org/confluence/display/Hive/Enhanced+Aggregation,+Cube,+Grouping+and+Rollup
+        return true;
+    }    
 }
