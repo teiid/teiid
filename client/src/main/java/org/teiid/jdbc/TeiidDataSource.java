ceead2f2d8fafb66b32b3ec76188d40837d9880f
shawkins <shawkins@redhat.com>
2014-06-13 10:33:27 -0400

    TEIID-3003

--- client/src/main/java/org/teiid/jdbc/TeiidDataSource.java
+++ client/src/main/java/org/teiid/jdbc/TeiidDataSource.java
@@ -142,6 +142,10 @@ public class TeiidDataSource extends BaseDataSource {
         if (this.getDiscoveryStrategy() != null) {
         	props.setProperty(TeiidURL.CONNECTION.DISCOVERY_STRATEGY, this.getDiscoveryStrategy());
         }
+        
+        if (this.encryptRequests) {
+        	props.setProperty(TeiidURL.CONNECTION.ENCRYPT_REQUESTS, Boolean.TRUE.toString());
+        }
 
         return props;
     }
