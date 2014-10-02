3e062020f249c0df764c7c80a43d51e7f28253f6
Tom Arnold <tom9729@gmail.com>
2014-10-02 02:53:40 -0400

    Changed to verify checksums for packages and made handling of packages a little more robust.

--- install.sh
+++ install.sh
@@ -7,10 +7,21 @@ TEIID_VERSION=$(xmllint --xpath '/*/*[local-name()="version"]/text()' pom.xml)
 TEIID_DIST=teiid-${TEIID_VERSION}-jboss-dist.zip
 
 SOURCES=(
-    "http://download.jboss.org/jbosseap/6/jboss-eap-6.1.0.Alpha/jboss-eap-6.1.0.Alpha.zip"
-    "http://sourceforge.net/projects/resteasy/files/Resteasy%20JAX-RS/2.3.6.Final/resteasy-jaxrs-2.3.6.Final-all.zip"
-    "http://sourceforge.net/projects/teiid/files/webconsole/1.2/Final/teiid-console-dist-1.2.0.Final-jboss-as7.zip"
+    "jboss-eap.zip"
+    "resteasy-jaxrs.zip"
+    "teiid-console-dist.zip"
 )
+
+declare -A SOURCE_URL SOURCE_SHA1
+
+SOURCE_URL["jboss-eap.zip"]="http://download.jboss.org/jbosseap/6/jboss-eap-6.1.0.Alpha/jboss-eap-6.1.0.Alpha.zip"
+SOURCE_SHA1["jboss-eap.zip"]="630d81f83b851077e3ad129924502bbdf0c1552a"
+
+SOURCE_URL["resteasy-jaxrs.zip"]="http://sourceforge.net/projects/resteasy/files/Resteasy%20JAX-RS/2.3.6.Final/resteasy-jaxrs-2.3.6.Final-all.zip"
+SOURCE_SHA1["resteasy-jaxrs.zip"]="cfcb2aaa60cd954d04e73cc7e99509ec38b5538a"
+
+SOURCE_URL["teiid-console-dist.zip"]="http://sourceforge.net/projects/teiid/files/webconsole/1.2/Final/teiid-console-dist-1.2.0.Final-jboss-as7.zip"
+SOURCE_SHA1["teiid-console-dist.zip"]="7b57b77520f2894b0f48a385f7dcff03898bb514"
 
 set -e
 
@@ -31,10 +42,35 @@ mkdir -p install
 cd install
 
 ## Download extra packages for server.
-echo "Downloading extra packages..."
-wget -nc ${SOURCES[@]}
-ln -sf ../build/target/$TEIID_DIST .
+echo "Downloading packages..."
 echo
+for NAME in "${SOURCES[@]}";
+do
+    URL="${SOURCE_URL[$NAME]}"
+    
+    ## Download if doesn't exist.
+    if [ ! -e "$NAME" ];
+    then	
+	echo "Downloading $NAME..."
+	wget -O "$NAME" "$URL"
+	echo
+    fi    
+done
+
+## Verify sources match checksums.
+echo "Verifying checksums..."
+echo
+for NAME in "${SOURCES[@]}";
+do
+    SHA1="${SOURCE_SHA1[$NAME]}"
+    
+    ## Verify checksum.
+    echo "$SHA1 $NAME" | sha1sum -c -
+    echo
+done
+
+## Create softlink for Teiid dist.
+ln -sf ../build/target/$TEIID_DIST .
 
 rm -rf temp
 mkdir temp
