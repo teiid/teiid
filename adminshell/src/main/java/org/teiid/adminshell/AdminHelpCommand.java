bea99712217013fcedc054c9a3dc40b76976b4d1
shawkins <shawkins@redhat.com>
2014-11-12 21:05:33 -0500

    TEIID-3213 adding in aliases for backwards compatibility

--- adminshell/src/main/java/org/teiid/adminshell/AdminHelpCommand.java
+++ adminshell/src/main/java/org/teiid/adminshell/AdminHelpCommand.java
@@ -22,8 +22,10 @@
 
 package org.teiid.adminshell;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.codehaus.groovy.tools.shell.Command;
 import org.codehaus.groovy.tools.shell.CommandSupport;
 import org.codehaus.groovy.tools.shell.Groovysh;
 
@@ -33,9 +35,23 @@ public class AdminHelpCommand extends CommandSupport {
 		super(shell, "adminhelp", "\\ah");  //$NON-NLS-1$ //$NON-NLS-2$
 		
 		//hook to introduce default imports
-		final String[] imports = GroovyAdminConsole.IMPORTS.split("\n");
+		final String[] imports = GroovyAdminConsole.IMPORTS.split("\n"); //$NON-NLS-1$
 		for(String aimport : imports){
 			shell.execute(aimport);
+		}
+		//for backwards compatibility add aliases to 1.7 Groovy commands
+		for (Command cmd :new ArrayList<Command>(shell.getRegistry().commands())) {
+			if (cmd.getHidden()) {
+				continue;
+			}
+			String name = cmd.getName();
+			if (name.startsWith(":")) { //$NON-NLS-1$
+				shell.execute(":alias " + name.substring(1) + " " + name); //$NON-NLS-1$ //$NON-NLS-2$
+				String shortCut = cmd.getShortcut();
+				if (shortCut.startsWith(":")) { //$NON-NLS-1$
+					shell.execute(":alias \\" + shortCut.substring(1) + " " + name); //$NON-NLS-1$ //$NON-NLS-2$
+				}
+			}
 		}
 	}
 
