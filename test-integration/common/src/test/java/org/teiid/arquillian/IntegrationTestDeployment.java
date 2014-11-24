5ffcc874f38fb3a488dff012bcda4d6f17101397
Van Halbert <vhalbert@redhat.com>
2014-11-24 17:12:15 -0600

    Fix test integration and fixed transitive dependencies that need to be specified

--- test-integration/common/src/test/java/org/teiid/arquillian/IntegrationTestDeployment.java
+++ test-integration/common/src/test/java/org/teiid/arquillian/IntegrationTestDeployment.java
@@ -239,7 +239,9 @@ public class IntegrationTestDeployment {
 
 	@Test
 	public void testVDBConnectionType() throws Exception {
-		admin.deploy("bqt.vdb", new FileInputStream(UnitTestUtil.getTestDataFile("bqt.vdb")));			
+		admin.deploy("bqt.vdb", new FileInputStream(UnitTestUtil.getTestDataFile("bqt.vdb")));	
+		
+		AdminUtil.waitForVDBLoad(admin, "bqt2", 1, 3);	
 		
 		VDB vdb = admin.getVDB("bqt", 1);
 		Model model = vdb.getModels().get(0);
@@ -268,6 +270,8 @@ public class IntegrationTestDeployment {
 		}
 
 		admin.deploy("bqt2.vdb", new FileInputStream(UnitTestUtil.getTestDataFile("bqt2.vdb")));
+		AdminUtil.waitForVDBLoad(admin, "bqt2", 1, 3);	
+
 		admin.updateSource("bqt", 2, "Source", "h2", "java:jboss/datasources/ExampleDS");
 		
 		conn = TeiidDriver.getInstance().connect("jdbc:teiid:bqt@mm://localhost:31000;user=user;password=user", null);
@@ -317,6 +321,7 @@ public class IntegrationTestDeployment {
 		s = sessions.iterator().next();
 
 		admin.terminateSession(s.getSessionId());
+		Thread.sleep(2000);
 		sessions = admin.getSessions();
 		assertEquals (0, sessions.size());			
 		conn.close();
@@ -462,7 +467,8 @@ public class IntegrationTestDeployment {
 	@Test
 	public void testDataRoleMapping() throws Exception{
 		admin.deploy("bqt2.vdb", new FileInputStream(UnitTestUtil.getTestDataFile("bqt2.vdb")));			
-		
+		AdminUtil.waitForVDBLoad(admin, "bqt2", 1, 3);
+
 		VDB vdb = admin.getVDB("bqt", 2);
 		Model model = vdb.getModels().get(0);
 		admin.assignToModel("bqt", 2, model.getName(), "Source", "h2", "java:jboss/datasources/ExampleDS");
