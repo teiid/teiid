05b003f20b5537b57761d852d2d84262cac301ac
felias <felias@redhat.com>
2014-12-02 18:18:55 +0100

    Improved behaviour of String columns in gooole translator

--- connectors/translator-google/src/main/java/org/teiid/translator/google/visitor/SpreadsheetInsertVisitor.java
+++ connectors/translator-google/src/main/java/org/teiid/translator/google/visitor/SpreadsheetInsertVisitor.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.teiid.core.types.DataTypeManager;
 import org.teiid.language.ColumnReference;
 import org.teiid.language.Expression;
 import org.teiid.language.ExpressionValueSource;
@@ -16,84 +17,83 @@ import org.teiid.language.visitor.SQLStringVisitor;
 import org.teiid.resource.adapter.google.common.SpreadsheetOperationException;
 import org.teiid.resource.adapter.google.metadata.SpreadsheetInfo;
 
-
 /**
  * Translates SQL INSERT commands
  * 
  * @author felias
- *
+ * 
  */
-public class SpreadsheetInsertVisitor extends SQLStringVisitor{
+public class SpreadsheetInsertVisitor extends SQLStringVisitor {
 	private String worksheetKey;
 	private List<String> columns;
 	private List<String> values;
-	private Map<String,String> columnNameValuePair;
+	private Map<String, String> columnNameValuePair;
 	SpreadsheetInfo info;
 	private String worksheetTitle;
-	
-	public SpreadsheetInsertVisitor(SpreadsheetInfo info){
-		this.info=info;
-		columnNameValuePair=new HashMap<String, String>();
-		columns=new ArrayList<String>();
-		values=new ArrayList<String>();
+
+	public SpreadsheetInsertVisitor(SpreadsheetInfo info) {
+		this.info = info;
+		columnNameValuePair = new HashMap<String, String>();
+		columns = new ArrayList<String>();
+		values = new ArrayList<String>();
 	}
-	
-	public void visit(Insert obj){
+
+	public void visit(Insert obj) {
 		worksheetTitle = obj.getTable().getName();
 		if (obj.getTable().getMetadataObject().getNameInSource() != null) {
 			worksheetTitle = obj.getTable().getMetadataObject().getNameInSource();
 		}
-		worksheetKey=info.getWorksheetByName(worksheetTitle).getId();
+		worksheetKey = info.getWorksheetByName(worksheetTitle).getId();
 		super.visit(obj);
-		for(int i=0;i<values.size() && i<columns.size();i++){
+		for (int i = 0; i < values.size() && i < columns.size(); i++) {
 			columnNameValuePair.put(columns.get(i), values.get(i));
 		}
 	}
-	
 
 	@Override
 	public void visit(ColumnReference obj) {
 		columns.add(obj.getMetadataObject().getName());
 		super.visit(obj);
 	}
-	
+
 	@Override
-	public void visit(ExpressionValueSource obj) {	
-		for(Expression e: obj.getValues()){
-			if (!(e instanceof Literal)){
+	public void visit(ExpressionValueSource obj) {
+		for (Expression e : obj.getValues()) {
+			if (!(e instanceof Literal)) {
 				throw new SpreadsheetOperationException("Only literals are allowed in the values section");
 			}
 		}
-		super.visit(obj);	
-		
-		
+		super.visit(obj);
+
 	}
-    public void visit(Literal obj) {
-    	if (obj.getValue() == null) {
-            buffer.append(NULL);
-            return;
-        }  
-    	 Class<?> type = obj.getType();
-    	if(Number.class.isAssignableFrom(type)) {
-    		values.add(obj.getValue().toString());   
-            return;
-        }	
-    	else { 
-        	values.add(obj.getValue().toString());   
-        	return;
-        }
-    }
+
+	public void visit(Literal obj) {
+		if (obj.getValue() == null) {
+			buffer.append(NULL);
+			return;
+		}
+		Class<?> type = obj.getType();
+		if (Number.class.isAssignableFrom(type)) {
+			values.add(obj.getValue().toString());
+			return;
+		} else if (type.equals(DataTypeManager.DefaultDataClasses.STRING)) {
+			values.add("'"+obj.getValue().toString());
+		} else {
+			values.add(obj.getValue().toString());
+			return;
+		}
+	}
+
 	public String getWorksheetKey() {
 		return worksheetKey;
 	}
-	
+
 	public Map<String, String> getColumnNameValuePair() {
 		return columnNameValuePair;
 	}
-	
+
 	public String getWorksheetTitle() {
 		return worksheetTitle;
 	}
-
 
 }
--- connectors/translator-google/src/main/java/org/teiid/translator/google/visitor/SpreadsheetUpdateVisitor.java
+++ connectors/translator-google/src/main/java/org/teiid/translator/google/visitor/SpreadsheetUpdateVisitor.java
@@ -3,27 +3,28 @@ package org.teiid.translator.google.visitor;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.teiid.core.types.DataTypeManager;
 import org.teiid.language.SetClause;
 import org.teiid.language.Update;
 import org.teiid.language.visitor.SQLStringVisitor;
 import org.teiid.resource.adapter.google.common.UpdateSet;
 import org.teiid.resource.adapter.google.metadata.SpreadsheetInfo;
+
 /**
- * Translates SQL UPDATE commands  
+ * Translates SQL UPDATE commands
  * 
  * 
  * @author felias
- *
+ * 
  */
 public class SpreadsheetUpdateVisitor extends SpreadsheetCriteriaVisitor {
 
-	
 	public SpreadsheetUpdateVisitor(SpreadsheetInfo info) {
 		super(info);
 	}
 
 	private List<UpdateSet> changes;
-	
+
 	public void visit(Update obj) {
 		worksheetTitle = obj.getTable().getName();
 		changes = new ArrayList<UpdateSet>();
@@ -33,7 +34,11 @@ public class SpreadsheetUpdateVisitor extends SpreadsheetCriteriaVisitor {
 		worksheetKey = info.getWorksheetByName(worksheetTitle).getId();
 		for (SetClause s : obj.getChanges()) {
 			SQLStringVisitor.getSQLString(s.getValue());
-			changes.add(new UpdateSet(s.getSymbol().getName(), getStringValue(s.getValue())));
+			if (s.getSymbol().getType().equals(DataTypeManager.DefaultDataClasses.STRING)) {
+				changes.add(new UpdateSet(s.getSymbol().getName(),"'"+getStringValue(s.getValue())));
+			} else {
+				changes.add(new UpdateSet(s.getSymbol().getName(), getStringValue(s.getValue())));
+			}
 		}
 		if (obj.getWhere() != null) {
 			append(obj.getWhere());
@@ -41,7 +46,7 @@ public class SpreadsheetUpdateVisitor extends SpreadsheetCriteriaVisitor {
 		} else {
 			criteriaQuery = "";
 		}
-		
+
 	}
 
 	public List<UpdateSet> getChanges() {
@@ -51,5 +56,5 @@ public class SpreadsheetUpdateVisitor extends SpreadsheetCriteriaVisitor {
 	public void setChanges(List<UpdateSet> changes) {
 		this.changes = changes;
 	}
-	
+
 }
