3184d081a3f6b7c8a09631372ad7e8b303bea9d6
Ramesh Reddy <rareddy@jboss.org>
2015-02-19 15:12:00 -0600

    TEIID-3043: Fixing the script error

--- build/kits/docker/build-image.sh
+++ build/kits/docker/build-image.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo docker build -rm -t teiid/teiid:${project.version} .
+sudo docker build --rm -t teiid/teiid:${project.version} .
 
 echo "Want to push the image to Docker Hub (y/n) ?"
 
 read selection
 
-if [ $selection eq 'y']; then
+if [ $selection == "y" ]; then
     sudo docker login
     sudo docker push teiid/teiid:${project.version}
 fi
