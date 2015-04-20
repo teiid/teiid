06c57694e9fd1aeee853398ade486a70903c3919
shawkins <shawkins@redhat.com>
2015-04-20 11:43:53 -0400

    TEIID-3412 pulling and updating ucanaccess support

--- test-integration/common/src/test/java/org/teiid/arquillian/IntegrationTestDeployment.java
+++ test-integration/common/src/test/java/org/teiid/arquillian/IntegrationTestDeployment.java
@@ -186,7 +186,7 @@ public class IntegrationTestDeployment {
 	@Test
 	public void testTraslators() throws Exception {
 		Collection<? extends Translator> translators = admin.getTranslators();
-		assertEquals(translators.toString(), 46, translators.size());
+		assertEquals(translators.toString(), 47, translators.size());
 
 		JavaArchive jar = getLoopyArchive();
 		
