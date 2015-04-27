372be2ff149f77a4463d284270874e20432dddbd
kylinsoong <kylinsoong.1214@gmail.com>
2015-04-27 19:21:32 +0800

    TEIID-3431: add newDataSource method to EmbeddedHelper

--- runtime/src/main/java/org/teiid/runtime/EmbeddedHelper.java
+++ runtime/src/main/java/org/teiid/runtime/EmbeddedHelper.java
@@ -25,16 +25,41 @@ import java.io.File;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 
+import javax.resource.ResourceException;
+import javax.sql.DataSource;
 import javax.transaction.TransactionManager;
+
+import org.jboss.jca.adapters.jdbc.local.LocalManagedConnectionFactory;
+import org.jboss.jca.core.api.connectionmanager.pool.PoolConfiguration;
+import org.jboss.jca.core.connectionmanager.notx.NoTxConnectionManagerImpl;
+import org.jboss.jca.core.connectionmanager.pool.strategy.OnePool;
 
 import com.arjuna.ats.arjuna.common.ObjectStoreEnvironmentBean;
 import com.arjuna.ats.arjuna.common.arjPropertyManager;
 import com.arjuna.common.internal.util.propertyservice.BeanPopulator;
 
 /**
- * A Helper class for running Teiid Embedded. 
+ * The <code>EmbeddedHelper</code> is a Helper class for running Teiid Embedded. 
  * 
- * The 'getTransactionManager()' method return a Narayana JTA TransactionManager.
+ * The 'getTransactionManager()' method return a Narayana JTA TransactionManager which used to set to EmbeddedConfiguration:
+ * 
+ * <pre>
+ *  EmbeddedServer server = new EmbeddedServer();
+ *  ...
+ *  EmbeddedConfiguration config = new EmbeddedConfiguration();
+ *  config.setTransactionManager(EmbeddedHelper.getTransactionManager());
+ *  server.start(config);
+ * </pre>
+ * 
+ * The 'newDataSource' method return a ironjacamar WrapperDataSource which used to set EmbeddedServer's named connection factory:
+ * 
+ * <pre>
+ *  EmbeddedServer server = new EmbeddedServer();
+ *  ...
+ *  DataSource ds = EmbeddedHelper.newDataSource("${driver}", "${url}", "${user}", "${password}");
+ *  server.addConnectionFactory("name", ds);
+ *  ...
+ * </pre>
  * 
  *
  */
@@ -83,5 +108,23 @@ public class EmbeddedHelper {
 				return System.getProperty(name);
 			}});
 	}
+	
+	public static DataSource newDataSource(String driverClass, String connURL, String user, String password) throws ResourceException{
+
+		LocalManagedConnectionFactory mcf = new LocalManagedConnectionFactory();
+		
+		mcf.setDriverClass(driverClass);
+		mcf.setConnectionURL(connURL);
+		mcf.setUserName(user);
+		mcf.setPassword(password);
+		
+		NoTxConnectionManagerImpl cm = new NoTxConnectionManagerImpl();
+		OnePool pool = new OnePool(mcf, new PoolConfiguration(), false);
+		pool.setConnectionListenerFactory(cm);
+		cm.setPool(pool);
+		
+		return (DataSource) mcf.createConnectionFactory(cm);
+	}
+
 
 }
--- runtime/src/test/java/org/teiid/runtime/TestEmbeddedHelper.java
+++ runtime/src/test/java/org/teiid/runtime/TestEmbeddedHelper.java
@@ -22,11 +22,23 @@
 package org.teiid.runtime;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.resource.ResourceException;
+import javax.sql.DataSource;
 import javax.transaction.RollbackException;
 import javax.transaction.Status;
 import javax.transaction.TransactionManager;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestEmbeddedHelper {
@@ -91,6 +103,59 @@ public class TestEmbeddedHelper {
 		Thread.sleep(1000 * 5);
 		
 		tm.commit();
+	}
+	
+	@Test
+	public void testDataSource() throws ResourceException, SQLException {
+		
+		String driverClass = "org.h2.Driver";
+		String connURL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+		String user = "sa";
+		String password = "sa";
+
+		
+		DataSource ds = EmbeddedHelper.newDataSource(driverClass, connURL, user, password);
+		
+		assertNotNull(ds);
+		
+		Connection conn = ds.getConnection();
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT CURRENT_DATE()");
+		assertTrue(rs.next());
+		
+		rs.close();
+		stmt.close();
+		conn.close();
+
+	}
+	
+	@Test
+	@Ignore("testPoolMaxSize will block 30 seconds")
+	public void testPoolMaxSize() throws ResourceException, SQLException{
+		
+		String driverClass = "org.h2.Driver";
+		String connURL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+		String user = "sa";
+		String password = "sa";
+
+		// ironjacamar default max size is 20
+		List<Connection> list = new ArrayList<Connection>();
+		DataSource ds = EmbeddedHelper.newDataSource(driverClass, connURL, user, password);
+		try {
+			for(int i = 0 ; i < 21 ; i ++) {
+				list.add(ds.getConnection());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		assertEquals(20, list.size());
+		
+		for(Connection conn : list) {
+			conn.close();
+		}
+		
+		
 	}
 
 }
