cc4be94bc888b571e55160e283d16580a690ea5f
kylinsoong <kylinsoong.1214@gmail.com>
2015-06-18 11:19:29 +0800

    TEIID-3541: remove embedded kit

--- build/kits/embedded/examples/ldap-as-a-datasource/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TEIID_PATH=../../lib/*:../../optional/ldap/*
-
-javac -cp ${TEIID_PATH} src/org/teiid/example/*.java 
-
-java -cp ./src:${TEIID_PATH} org.teiid.example.TeiidEmbeddedLDAPDataSource "$@"
--- build/kits/embedded/examples/mongodb-as-a-datasource/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TEIID_PATH=../../lib/*:../../optional/mongodb/*:../../optional/jdbc/*
-
-javac -cp ${TEIID_PATH} src/org/teiid/example/*.java 
-
-java -cp ./src:${TEIID_PATH} org.teiid.example.TeiidEmbeddedMongoDBDataSource "$@"
