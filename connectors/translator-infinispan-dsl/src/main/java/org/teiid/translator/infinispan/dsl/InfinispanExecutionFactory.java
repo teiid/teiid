83349a58189109ed07da239df363361a74382635
shawkins <shawkins@redhat.com>
2015-08-18 08:52:32 -0400

    TEIID-3627 removing override

--- connectors/translator-infinispan-dsl/src/main/java/org/teiid/translator/infinispan/dsl/InfinispanExecutionFactory.java
+++ connectors/translator-infinispan-dsl/src/main/java/org/teiid/translator/infinispan/dsl/InfinispanExecutionFactory.java
@@ -141,11 +141,6 @@ public class InfinispanExecutionFactory extends
 	}
 	
 	@Override
-	public boolean supportsCompareCriteriaOrderedExclusive() {
-		return true;
-	}
-	
-	@Override
 	public boolean supportsLikeCriteria() {
 		return Boolean.TRUE.booleanValue();
 	}	
