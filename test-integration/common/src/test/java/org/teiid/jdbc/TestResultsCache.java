6c6e2938920b55f21cab16c0842b3aa02a4f4a27
shawkins <shawkins@redhat.com>
2015-12-02 15:21:08 -0500

    TEIID-3831 adding more options for controling result set caching scope

--- test-integration/common/src/test/java/org/teiid/jdbc/TestResultsCache.java
+++ test-integration/common/src/test/java/org/teiid/jdbc/TestResultsCache.java
@@ -28,13 +28,27 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.teiid.adminapi.impl.ModelMetaData;
 import org.teiid.core.util.UnitTestUtil;
+import org.teiid.language.Call;
+import org.teiid.language.QueryExpression;
+import org.teiid.metadata.RuntimeMetadata;
+import org.teiid.translator.CacheDirective.Scope;
+import org.teiid.translator.DataNotAvailableException;
+import org.teiid.translator.ExecutionContext;
+import org.teiid.translator.ExecutionFactory;
+import org.teiid.translator.ProcedureExecution;
+import org.teiid.translator.ResultSetExecution;
+import org.teiid.translator.TranslatorException;
 
 
 @SuppressWarnings("nls")
@@ -120,6 +134,127 @@ public class TestResultsCache {
 		assertTrue(rs.next());
 		assertEquals(148907, rs.getString(1).length());
 		assertFalse(rs.next());
+	}
+	
+	@Test public void testScope() throws Exception {
+		ModelMetaData mmd = new ModelMetaData();
+		mmd.setName("x");
+		mmd.addProperty("teiid_rel:determinism", "USER_DETERMINISTIC");
+		mmd.addSourceMapping("x", "x", null);
+		mmd.addSourceMetadata("ddl", "create foreign table t (c string); create foreign procedure p () returns table (c string);");
+		final AtomicBoolean setScope = new AtomicBoolean();
+		server.addTranslator("x", new ExecutionFactory() {
+			@Override
+			public boolean isSourceRequired() {
+				return false;
+			}
+			
+			@Override
+			public ResultSetExecution createResultSetExecution(
+					QueryExpression command, final ExecutionContext executionContext,
+					RuntimeMetadata metadata, Object connection)
+					throws TranslatorException {
+				return createProcedureExecution(null, executionContext, metadata, connection);
+			}
+			
+			@Override
+			public ProcedureExecution createProcedureExecution(Call command,
+					final ExecutionContext executionContext,
+					RuntimeMetadata metadata, Object connection)
+					throws TranslatorException {
+				return new ProcedureExecution() {
+					
+					boolean returned = false;
+					
+					@Override
+					public void execute() throws TranslatorException {
+						
+					}
+					
+					@Override
+					public void close() {
+						
+					}
+					
+					@Override
+					public void cancel() throws TranslatorException {
+						
+					}
+					
+					@Override
+					public List<?> next() throws TranslatorException, DataNotAvailableException {
+						if (setScope.get()) {
+							executionContext.setScope(Scope.SESSION); //prevent caching altogether
+						}
+						if (returned) {
+							return null;
+						}
+						returned = true;
+						return Arrays.asList(executionContext.getSession().getSessionId());
+					}
+					
+					@Override
+					public List<?> getOutputParameterValues()
+							throws TranslatorException {
+						return null;
+					}
+				};
+			}
+		});
+		server.deployVDB("x", mmd);
+		Connection c = server.getDriver().connect("jdbc:teiid:x;user=alice", null);
+		Statement s = c.createStatement();
+		ResultSet rs = s.executeQuery("/* cache */ select * from t");
+		assertTrue(rs.next());
+		String sessionid = rs.getString(1);
+		
+		//should be the same with same user/session
+		rs = s.executeQuery("/* cache */ select * from t");
+		assertTrue(rs.next());
+		assertEquals(sessionid, rs.getString(1));
+		c.close();
+
+		c = server.getDriver().connect("jdbc:teiid:x;user=alice", null);
+		s = c.createStatement();
+		rs = s.executeQuery("/* cache */ select * from t");
+		assertTrue(rs.next());
+		assertEquals(sessionid, rs.getString(1));
+		c.close();
+		
+		//for the final test
+		setScope.set(true);
+				
+		//should be different with another user
+		c = server.getDriver().connect("jdbc:teiid:x;user=bill", null);
+		s = c.createStatement();
+		rs = s.executeQuery("/* cache */ select * from t");
+		assertTrue(rs.next());
+		String sessionid1 = rs.getString(1);
+		c.close();
+		
+		assertNotEquals(sessionid, sessionid1);
+		
+		c = server.getDriver().connect("jdbc:teiid:x;user=bill", null);
+		s = c.createStatement();
+		rs = s.executeQuery("/* cache */ select * from t");
+		assertTrue(rs.next());
+		
+		//scope session should prevent reuse
+		assertNotEquals(sessionid1, rs.getString(1));
+		
+		setScope.set(false);
+		
+		rs = s.executeQuery("/* cache */ call p()");
+		assertTrue(rs.next());
+		sessionid = rs.getString(1);
+		c.close();
+		
+		c = server.getDriver().connect("jdbc:teiid:x;user=alice", null);
+		s = c.createStatement();
+		rs = s.executeQuery("/* cache */ call p()");
+		assertTrue(rs.next());
+		assertNotEquals(sessionid, rs.getString(1));
+		c.close();
 	}
 	
 }
