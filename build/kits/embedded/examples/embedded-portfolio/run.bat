20ebf5c355faed7848b3f23702ec6c01abe0c3a4
Ramesh Reddy <rareddy@jboss.org>
2015-12-14 10:37:13 -0600

    TEIID-3541: removing the embedded kit entirely from build

--- build/kits/embedded/examples/embedded-portfolio/run.bat
+++
@@ -1,6 +0,0 @@
-TEIID_PATH=..\..\lib\*;..\..\optional\file\*;..\..\optional\jdbc\*;..\..\optional\jdbc\h2\*;..\..\optional\jdbc\tm\*;..\..\optional\*
-
-javac -cp %TEIID_PATH% src\org\teiid\example\*.java 
-
-java -cp .\src;%TEIID_PATH% org.teiid.example.TeiidEmbeddedPortfolio %*
-
--- build/kits/embedded/examples/embedded-portfolio/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TEIID_PATH=../../lib/*:../../optional/file/*:../../optional/jdbc/*:../../optional/jdbc/h2/*:../../optional/*::../../optional/jdbc/tm/*
-
-javac -cp ${TEIID_PATH} src/org/teiid/example/*.java 
-
-java -cp ./src:${TEIID_PATH} org.teiid.example.TeiidEmbeddedPortfolio "$@"
