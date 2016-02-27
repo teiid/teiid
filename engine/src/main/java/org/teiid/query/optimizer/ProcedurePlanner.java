f459d7a2781ecefd0030ef7db05168b7dc8fd9b4
shawkins <shawkins@redhat.com>
2016-02-27 09:37:47 -0500

    TEIID-3995 making sure results aren't returned inappropriately

--- engine/src/main/java/org/teiid/query/optimizer/ProcedurePlanner.java
+++ engine/src/main/java/org/teiid/query/optimizer/ProcedurePlanner.java
@@ -243,7 +243,7 @@ public final class ProcedurePlanner implements CommandPlanner {
 				ProcessorPlan commandPlan = cmdStmt.getCommand().getProcessorPlan();                
                 
 				if (command.getType() == Command.TYPE_DYNAMIC){
-					instruction = new ExecDynamicSqlInstruction(parentProcCommand,((DynamicCommand)command), metadata, idGenerator, capFinder, cmdStmt.isReturnable() );
+					instruction = new ExecDynamicSqlInstruction(parentProcCommand,((DynamicCommand)command), metadata, idGenerator, capFinder, cmdStmt.isReturnable() && parentProcCommand.returnsResultSet());
 				}else{
 					CreateCursorResultSetInstruction cursor = new CreateCursorResultSetInstruction(null, commandPlan, getMode(parentProcCommand, cmdStmt, command));
 					if (cursor.getMode() == Mode.HOLD) {
