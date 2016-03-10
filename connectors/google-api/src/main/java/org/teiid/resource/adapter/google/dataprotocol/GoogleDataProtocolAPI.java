397210b26beb579eb2836aed7b9fc1d1a8d8a840
shawkins <shawkins@redhat.com>
2016-03-10 08:49:05 -0500

    TEIID-4034 ensuring that the calendar is cleared

--- connectors/google-api/src/main/java/org/teiid/resource/adapter/google/dataprotocol/GoogleDataProtocolAPI.java
+++ connectors/google-api/src/main/java/org/teiid/resource/adapter/google/dataprotocol/GoogleDataProtocolAPI.java
@@ -310,9 +310,8 @@ public class GoogleDataProtocolAPI {
 				List<Double> doubleVals = (List<Double>)object;
 				if (cal == null) {
 					cal = Calendar.getInstance();
-				} else {
-					cal.clear();
 				}
+				cal.clear();
 				cal.set(Calendar.YEAR, 1970);
 				cal.set(Calendar.MONTH, Calendar.JANUARY);
 				cal.set(Calendar.DAY_OF_MONTH, 1);
