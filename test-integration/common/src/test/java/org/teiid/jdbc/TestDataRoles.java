6609e77781a4a41c789844dc1e9b507592480db3
shawkins <shawkins@redhat.com>
2016-03-28 18:18:48 -0400

    TEIID-2476 adding a test for properties as well

--- test-integration/common/src/test/java/org/teiid/jdbc/TestDataRoles.java
+++ test-integration/common/src/test/java/org/teiid/jdbc/TestDataRoles.java
@@ -125,8 +125,8 @@ public class TestDataRoles {
 		es.deployVDB(new ByteArrayInputStream(new String("<vdb name=\"role-1\" version=\"1\">"
 				+ "<model name=\"s1\" type=\"virtual\">"
 				+ "<metadata type = \"DDL\"><![CDATA["
-				+ "CREATE VIEW t2 (col string primary key) as select 'a' as col;\n"
-				+ "CREATE VIEW t1 (col string, col_hidden string, foreign key (col) references t2) as select col, 'b' as col_hidden from t2;\n"
+				+ "CREATE VIEW t2 (col string primary key) options (x 'y') as select 'a' as col;\n"
+				+ "CREATE VIEW t1 (col string, col_hidden string, foreign key (col) references t2) options (x 'y1') as select col, 'b' as col_hidden from t2;\n"
 				+ "CREATE virtual procedure proc1 (param1 string) returns table (proc_col string) as begin end;\n"
 				+ "]]></metadata></model>"
 				+ "<model name=\"s2\" type=\"virtual\">"
@@ -178,7 +178,7 @@ public class TestDataRoles {
     	assertEquals("proc_col", rs.getString("name"));
     	assertFalse(rs.next());
     	
-    	rs = s.executeQuery("select * from sys.keys where tablename like 't_'");
+    	rs = s.executeQuery("select * from sys.keys where name = 'x'");
     	//nothing should be visible
     	assertFalse(rs.next());
     	
@@ -186,6 +186,10 @@ public class TestDataRoles {
     	//nothing should be visible
     	assertFalse(rs.next());
     	
+    	rs = s.executeQuery("select * from sys.properties where name = 'x'");
+    	assertTrue(rs.next());
+    	assertEquals("y1", rs.getString("Value"));
+    	assertFalse(rs.next());
 	}
 	
 }
