df6000e18b37a5298d014396f792d587206ced29
shawkins <shawkins@redhat.com>
2016-06-10 15:26:19 -0400

    TEIID-4238: Remove need for quotes around cursor name declarations/fetches (removing the need for quoting)

--- test-integration/common/src/test/java/org/teiid/transport/TestODBCSocketTransport.java
+++ test-integration/common/src/test/java/org/teiid/transport/TestODBCSocketTransport.java
@@ -379,17 +379,30 @@ public static class AnonSSLSocketFactory extends SSLSocketFactory {
 		Statement stmt = conn.createStatement();
 		ExtendedQueryExectutorImpl.simplePortal = "foo";
 		try {
-			assertFalse(stmt.execute("declare \"foo\" cursor for select * from pg_proc;"));
+			assertFalse(stmt.execute("declare \"foo\" cursor for select * from pg_proc limit 11;"));
+			
+			//should get a single row
 			stmt.execute("fetch \"foo\"");
-			assertFalse(stmt.execute("move 5 in \"foo\""));
-			stmt.execute("fetch 10 in \"foo\"");
 			ResultSet rs = stmt.getResultSet();
 			int rowCount = 0;
 			while (rs.next()) {
 				rowCount++;
 			}
 
+			assertEquals(1, rowCount);
+			
+			//move 5
+			assertFalse(stmt.execute("move 5 in \"foo\""));
+			
+			//fetch 10, but only 5 are left
+			stmt.execute("fetch 10 in \"foo\"");
+			rs = stmt.getResultSet();
+			rowCount = 0;
+			while (rs.next()) {
+				rowCount++;
+			}
 			assertEquals(5, rowCount);
+			
 			stmt.execute("close \"foo\"");
 			
 			//start a new cursor and check failure
@@ -405,6 +418,40 @@ public static class AnonSSLSocketFactory extends SSLSocketFactory {
 		}
 		
 	}	
+	
+	@Test public void testCursorUnquoted() throws Exception {
+		Statement stmt = conn.createStatement();
+		ExtendedQueryExectutorImpl.simplePortal = "foo";
+		try {
+			assertFalse(stmt.execute("declare foo cursor for select * from pg_proc limit 11;"));
+			
+			//should get a single row
+			stmt.execute("fetch foo");
+			ResultSet rs = stmt.getResultSet();
+			int rowCount = 0;
+			while (rs.next()) {
+				rowCount++;
+			}
+			assertEquals(1, rowCount);
+			
+			//move 5
+			assertFalse(stmt.execute("move 5 in foo"));
+			
+			//fetch 10, but only 5 are left
+			stmt.execute("fetch 10 in \"foo\"");
+			rs = stmt.getResultSet();
+			rowCount = 0;
+			while (rs.next()) {
+				rowCount++;
+			}
+			assertEquals(5, rowCount);
+			
+			stmt.execute("close foo");			
+		} finally {
+			ExtendedQueryExectutorImpl.simplePortal = null;
+		}
+		
+	}
 	
 	@Test public void testScrollCursor() throws Exception {
 		Statement stmt = conn.createStatement();
