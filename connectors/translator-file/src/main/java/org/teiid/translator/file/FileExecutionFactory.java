58c0bece8943855167f9b3d5844f509bb99cc84c
rafael coutinho <rafael.coutinho@gmail.com>
2016-06-30 17:16:59 -0300

    Add deleteFile call to file translator

--- connectors/translator-file/src/main/java/org/teiid/translator/file/FileExecutionFactory.java
+++ connectors/translator-file/src/main/java/org/teiid/translator/file/FileExecutionFactory.java
@@ -140,6 +140,7 @@ public class FileExecutionFactory extends ExecutionFactory<ConnectionFactory, Fi
 	public static final String GETTEXTFILES = "getTextFiles"; //$NON-NLS-1$
 	public static final String GETFILES = "getFiles"; //$NON-NLS-1$
 	public static final String SAVEFILE = "saveFile"; //$NON-NLS-1$
+	public static final String DELETEFILE = "deleteFile"; //$NON-NLS-1$
 	
 	private Charset encoding = Charset.defaultCharset();
 	private boolean exceptionIfFileNotFound = true;
@@ -221,6 +222,46 @@ public class FileExecutionFactory extends ExecutionFactory<ConnectionFactory, Fi
 					return Collections.emptyList();
 				}
 			};
+		}else if (command.getProcedureName().equalsIgnoreCase(DELETEFILE)) {
+			return new ProcedureExecution() {
+				
+				@Override
+				public void execute() throws TranslatorException {
+					String filePath = (String)command.getArguments().get(0).getArgumentValue().getValue();
+					
+					if ( filePath == null) {
+						throw new TranslatorException(UTIL.getString("non_null")); //$NON-NLS-1$
+					}
+					LogManager.logDetail(LogConstants.CTX_CONNECTOR, "Deleting", filePath); //$NON-NLS-1$
+					
+					try {
+						if(!fc.getFile(filePath).delete()){
+							throw new TranslatorException(new IOException(), UTIL.getString("error_deleting")); //$NON-NLS-1$
+						}
+						
+					} catch (ResourceException e) {
+						throw new TranslatorException(e, UTIL.getString("error_deleting")); //$NON-NLS-1$
+					}
+				}
+				
+				@Override
+				public void close() {
+				}
+				
+				@Override
+				public void cancel() throws TranslatorException {
+				}
+				
+				@Override
+				public List<?> next() throws TranslatorException, DataNotAvailableException {
+					return null;
+				}
+				
+				@Override
+				public List<?> getOutputParameterValues() throws TranslatorException {
+					return Collections.emptyList();
+				}
+			};
 		}
 		return new FileProcedureExecution(command, fc);
 	}
@@ -246,6 +287,11 @@ public class FileExecutionFactory extends ExecutionFactory<ConnectionFactory, Fi
 		metadataFactory.addProcedureParameter("filePath", TypeFacility.RUNTIME_NAMES.STRING, Type.In, p2); //$NON-NLS-1$
 		param = metadataFactory.addProcedureParameter("file", TypeFacility.RUNTIME_NAMES.OBJECT, Type.In, p2); //$NON-NLS-1$
 		param.setAnnotation("The contents to save.  Can be one of CLOB, BLOB, or XML"); //$NON-NLS-1$
+		
+		Procedure p3 = metadataFactory.addProcedure(DELETEFILE);
+		p3.setAnnotation("Delete the given file path. "); //$NON-NLS-1$
+		metadataFactory.addProcedureParameter("filePath", TypeFacility.RUNTIME_NAMES.STRING, Type.In, p3); //$NON-NLS-1$	
+		
 	} 
 	
 	@Override
