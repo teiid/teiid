a59e3f38a13dea7bc18fb1542dad32c377fded31
shawkins <shawkins@redhat.com>
2016-07-07 13:02:58 -0400

    TEIID-4304 turning off subquery on clause support entirely

--- connectors/translator-jdbc/src/main/java/org/teiid/translator/jdbc/oracle/OracleExecutionFactory.java
+++ connectors/translator-jdbc/src/main/java/org/teiid/translator/jdbc/oracle/OracleExecutionFactory.java
@@ -1112,6 +1112,6 @@ public class OracleExecutionFactory extends JDBCExecutionFactory {
     
     @Override
     public boolean supportsSubqueryInOn() {
-    	return getVersion().compareTo(TWELVE) >= 0;
+    	return false; //even oracle 12 still has issues if a with clause is also in the source query
     }
 }
