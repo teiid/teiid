30639e3f3f71dd82547057898e8ae0620679147f
kylinsoong <kylinsoong.1214@gmail.com>
2016-11-04 14:36:56 +0800

    TEIID-4545: add a new PhoenixExecutionFactory and support join

--- connectors/translator-hbase/src/main/java/org/teiid/translator/hbase/HBaseExecutionFactory.java
+++ connectors/translator-hbase/src/main/java/org/teiid/translator/hbase/HBaseExecutionFactory.java
@@ -21,8 +21,6 @@
  */
 package org.teiid.translator.hbase;
 
-import static org.teiid.translator.TypeFacility.RUNTIME_NAMES.*;
-
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -43,18 +41,16 @@ import org.teiid.language.SubqueryComparison.Quantifier;
 import org.teiid.metadata.RuntimeMetadata;
 import org.teiid.translator.ExecutionContext;
 import org.teiid.translator.ResultSetExecution;
-import org.teiid.translator.SourceSystemFunctions;
 import org.teiid.translator.Translator;
 import org.teiid.translator.TranslatorException;
 import org.teiid.translator.TypeFacility;
-import org.teiid.translator.jdbc.AliasModifier;
-import org.teiid.translator.jdbc.JDBCExecutionFactory;
 import org.teiid.translator.jdbc.JDBCMetdataProcessor;
 import org.teiid.translator.jdbc.JDBCUpdateExecution;
 import org.teiid.util.Version;
 
+@Deprecated
 @Translator(name="hbase", description="HBase Translator, reads and writes the data to HBase")
-public class HBaseExecutionFactory extends JDBCExecutionFactory {
+public class HBaseExecutionFactory extends PhoenixExecutionFactory {
     
     public static String HBASE = "hbase"; //$NON-NLS-1$
     public static final Version V_4_8 = Version.getVersion("4.8"); //$NON-NLS-1$
@@ -68,74 +64,7 @@ public class HBaseExecutionFactory extends JDBCExecutionFactory {
     @Override
     public void start() throws TranslatorException {
         super.start();
-        
-        registerFunctionModifier(SourceSystemFunctions.SUBSTRING, new AliasModifier("SUBSTR")); //$NON-NLS-1$
-        registerFunctionModifier(SourceSystemFunctions.UCASE, new AliasModifier("UPPER")); //$NON-NLS-1$
-        registerFunctionModifier(SourceSystemFunctions.LCASE, new AliasModifier("LOWER")); //$NON-NLS-1$
-        registerFunctionModifier(SourceSystemFunctions.LOCATE, new AliasModifier("INSTR")); //$NON-NLS-1$
-        registerFunctionModifier(SourceSystemFunctions.PARSETIMESTAMP, new AliasModifier("TO_TIMESTAMP")); //$NON-NLS-1$
-        registerFunctionModifier(SourceSystemFunctions.CURTIME, new AliasModifier("CURRENT_TIME")); //$NON-NLS-1$
-        registerFunctionModifier(SourceSystemFunctions.LOG, new AliasModifier("LN")); //$NON-NLS-1$
-        registerFunctionModifier(SourceSystemFunctions.LOG10, new AliasModifier("LOG")); //$NON-NLS-1$
-        registerFunctionModifier(SourceSystemFunctions.PARSEBIGDECIMAL, new AliasModifier("TO_NUMBER")); //$NON-NLS-1$
-        
-        addPushDownFunction(HBASE, "REVERSE", STRING, STRING); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "REGEXP_SUBSTR", STRING, STRING, STRING, INTEGER); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "REGEXP_REPLACE", STRING, STRING, STRING, STRING); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "REGEXP_SPLIT", OBJECT, STRING, STRING); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "TO_DATE", DATE, STRING, STRING, STRING); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "TO_TIME", TIME, STRING, STRING, STRING); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "TIMEZONE_OFFSET", INTEGER, STRING, DATE); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "TIMEZONE_OFFSET", INTEGER, STRING, TIME); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "TIMEZONE_OFFSET", INTEGER, STRING, TIMESTAMP); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "CONVERT_TZ", DATE, DATE, STRING, STRING); //$NON-NLS-1$
-        addPushDownFunction(HBASE, "CONVERT_TZ", TIME, TIME, STRING, STRING); //$NON-NLS-1$
     }
-    
-    
-
-    @Override
-    public List<String> getSupportedFunctions() {
-        
-        List<String> supportedFunctions = new ArrayList<String>();
-        supportedFunctions.addAll(super.getSupportedFunctions());
-        
-        supportedFunctions.add(SourceSystemFunctions.SUBSTRING);
-        supportedFunctions.add(SourceSystemFunctions.LOCATE);
-        supportedFunctions.add(SourceSystemFunctions.TRIM);
-        supportedFunctions.add(SourceSystemFunctions.LTRIM);
-        supportedFunctions.add(SourceSystemFunctions.RTRIM);
-        supportedFunctions.add(SourceSystemFunctions.LPAD);
-        supportedFunctions.add(SourceSystemFunctions.LENGTH);
-        supportedFunctions.add(SourceSystemFunctions.UCASE);
-        supportedFunctions.add(SourceSystemFunctions.LCASE);
-        
-        supportedFunctions.add(SourceSystemFunctions.PARSETIMESTAMP);
-        supportedFunctions.add(SourceSystemFunctions.CURTIME);
-        supportedFunctions.add(SourceSystemFunctions.NOW);
-        supportedFunctions.add(SourceSystemFunctions.YEAR);
-        supportedFunctions.add(SourceSystemFunctions.MONTH);
-        supportedFunctions.add(SourceSystemFunctions.WEEK);
-        supportedFunctions.add(SourceSystemFunctions.DAYOFMONTH);
-        supportedFunctions.add(SourceSystemFunctions.HOUR);
-        supportedFunctions.add(SourceSystemFunctions.MINUTE);
-        supportedFunctions.add(SourceSystemFunctions.SECOND);
-        
-        supportedFunctions.add(SourceSystemFunctions.SIGN);
-        supportedFunctions.add(SourceSystemFunctions.ABS);
-        supportedFunctions.add(SourceSystemFunctions.SQRT);
-        supportedFunctions.add(SourceSystemFunctions.EXP);
-        supportedFunctions.add(SourceSystemFunctions.POWER);
-        supportedFunctions.add(SourceSystemFunctions.LOG);
-        supportedFunctions.add(SourceSystemFunctions.LOG10);
-        supportedFunctions.add(SourceSystemFunctions.RAND);
-        supportedFunctions.add(SourceSystemFunctions.ROUND);
-        supportedFunctions.add(SourceSystemFunctions.FLOOR);
-        supportedFunctions.add(SourceSystemFunctions.PARSEBIGDECIMAL);
-        
-        return supportedFunctions;
-    }
-
 
     @Override
     public ResultSetExecution createResultSetExecution(QueryExpression command
