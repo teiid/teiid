39173197d12f1a878aaf18eb53600f3aba14db41
vhalbert <vhalbert@jboss.org>
2016-11-18 15:44:55 -0500

    TEIID-4590 change the getAll logic in hotrod and libmode connectors to search the cache based on the pojo defined for that resource-adapter

--- connectors/infinispan/connector-infinispan-hotrod/src/main/java/org/teiid/resource/adapter/infinispan/hotrod/DSLSearch.java
+++ connectors/infinispan/connector-infinispan-hotrod/src/main/java/org/teiid/resource/adapter/infinispan/hotrod/DSLSearch.java
@@ -134,9 +134,20 @@ public final class DSLSearch implements SearchType  {
 		
 	}
 	
+	public List<Object> getAll() throws TranslatorException  {	
+		QueryBuilder qb = getQueryBuilder(conn);
+		
+		Query query = qb.build();
+		List<Object> results = query.list();
+		if (results == null) {
+               return Collections.emptyList();
+		}
+
+		return results;
+	}
+	
 	private List<Object> performSearch(ObjectVisitor visitor) throws TranslatorException  {			
 
-		
 		Condition where = visitor.getWhereCriteria();
 		OrderBy orderby = visitor.getOrderBy();		
 
@@ -172,23 +183,12 @@ public final class DSLSearch implements SearchType  {
 				return Collections.emptyList();
 			}
 			
-		} else if (orderby != null || visitor.getLimit() > 0) {
+		} else  {
 		   query = qb.build();
            results = query.list();
            if (results == null) {
                    return Collections.emptyList();
            }
-
-		} else {
-			results = new ArrayList<Object>();
-			Collection<Object> all = conn.getAll();
-			results.addAll(all);
-			return results;			
-//		    results = new ArrayList<Object>();
-//			RemoteCache<?, Object> c = (RemoteCache<?, Object>) conn.getCache();
-//		    for (Object id : c.keySet()) {
-//		          results. add(c.get(id));
-//		    }
 		}
 
 		return results;
