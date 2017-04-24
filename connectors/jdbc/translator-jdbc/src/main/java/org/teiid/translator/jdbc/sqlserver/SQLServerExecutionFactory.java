6557f475756c8d917dd8f3156037c4be186547de
shawkins <shawkins@redhat.com>
2017-04-24 08:40:29 -0400

    TEIID-4874 changing default support for full outer join

--- connectors/jdbc/translator-jdbc/src/main/java/org/teiid/translator/jdbc/sqlserver/SQLServerExecutionFactory.java
+++ connectors/jdbc/translator-jdbc/src/main/java/org/teiid/translator/jdbc/sqlserver/SQLServerExecutionFactory.java
@@ -76,6 +76,7 @@ public class SQLServerExecutionFactory extends SybaseExecutionFactory {
 	
 	//TEIID-31 remove mod modifier for SQL Server 2008
 	public SQLServerExecutionFactory() {
+	    setSupportsFullOuterJoins(true);
 		setMaxInCriteriaSize(JDBCExecutionFactory.DEFAULT_MAX_IN_CRITERIA);
 		setMaxDependentInPredicates(2);
 	}
