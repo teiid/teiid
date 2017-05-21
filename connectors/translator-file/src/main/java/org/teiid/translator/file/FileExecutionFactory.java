f81d69e1bd7025f2096bf648b4b76432c4af50a1
shawkins <shawkins@redhat.com>
2017-05-21 19:07:14 -0400

    TEIID-4857 adding a backwards compatibility check # Conflicts: #	connectors/translator-file/src/main/java/org/teiid/translator/file/FileExecutionFactory.java #	connectors/translator-file/src/test/java/org/teiid/translator/file/TestFileExecutionFactory.java

--- connectors/translator-file/src/main/java/org/teiid/translator/file/FileExecutionFactory.java
+++ connectors/translator-file/src/main/java/org/teiid/translator/file/FileExecutionFactory.java
@@ -130,14 +130,16 @@ public class FileExecutionFactory extends ExecutionFactory<ConnectionFactory, Fi
 			}
 			result.add(value);
 			result.add(file.getName());
-			result.add(new Timestamp(file.lastModified()));
-            try {
-                Map<String, Object> attributes = Files.readAttributes(file.toPath(), "size,creationTime"); //$NON-NLS-1$
-                result.add(new Timestamp(((FileTime)attributes.get("creationTime")).toMillis())); //$NON-NLS-1$
-                result.add(attributes.get("size")); //$NON-NLS-1$
-            } catch (IOException e) {
-                throw new TranslatorException(e);
-            }
+			if (command.getMetadataObject().getResultSet().getColumns().size() > 2) {
+    			result.add(new Timestamp(file.lastModified()));
+                try {
+                    Map<String, Object> attributes = Files.readAttributes(file.toPath(), "size,creationTime"); //$NON-NLS-1$
+                    result.add(new Timestamp(((FileTime)attributes.get("creationTime")).toMillis())); //$NON-NLS-1$
+                    result.add(attributes.get("size")); //$NON-NLS-1$
+                } catch (IOException e) {
+                    throw new TranslatorException(e);
+                }
+			}
 			return result;
 		}
 
