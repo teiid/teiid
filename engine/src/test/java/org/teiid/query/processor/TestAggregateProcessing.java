827eb3813374509466cdbfcdda9b94f9fe91289e
shawkins <shawkins@redhat.com>
2017-06-05 15:45:50 -0400

    BZ1417982 + TEIID-4875(reprise): addressing another issue with multiple decompositions

--- engine/src/test/java/org/teiid/query/processor/TestAggregateProcessing.java
+++ engine/src/test/java/org/teiid/query/processor/TestAggregateProcessing.java
@@ -449,6 +449,37 @@ public class TestAggregateProcessing {
 
         helpProcess(plan, dataManager, expected);
     }
+	
+    @Test public void testMultiJoinCriteria1a() throws Exception {
+        String sql = "SELECT max(t3.e4), max(t2.e4) as s FROM pm1.g1 as t1, pm1.g2 as t2, pm1.g3 as t3, pm1.g4 as t4, pm2.g1 as t5 "
+                + "WHERE t1.e1 = t2.e1 and (t2.e2 = t3.e2 and t1.e3 || t2.e3 = t3.e3) and t3.e3 = t4.e3 and t4.e4 = t5.e4"; //$NON-NLS-1$
+
+        List[] expected = new List[] {
+                Arrays.asList(null, null)
+        };
+
+        HardcodedDataManager dataManager = new HardcodedDataManager(true);
+        
+        BasicSourceCapabilities caps = TestAggregatePushdown.getAggregateCapabilities();
+        //caps.setCapabilitySupport(Capability.QUERY_GROUP_BY, false);
+        caps.setCapabilitySupport(Capability.QUERY_FROM_JOIN_INNER, false);
+        caps.setCapabilitySupport(Capability.QUERY_FROM_JOIN_OUTER, false);
+        caps.setFunctionSupport("convert", true);
+
+        ProcessorPlan plan = helpGetPlan(sql, RealMetadataFactory.example1Cached(), new DefaultCapabilitiesFinder(caps));
+        
+        dataManager.addData("SELECT DISTINCT g_0.e3 AS c_0, g_0.e4 AS c_1 FROM pm1.g4 AS g_0 ORDER BY c_0", Arrays.asList(false, 1.0));
+        dataManager.addData("SELECT DISTINCT g_0.e4 AS c_0 FROM pm2.g1 AS g_0 ORDER BY c_0", Arrays.asList(1.0));
+        dataManager.addData("SELECT DISTINCT g_0.e1 AS c_0, g_0.e3 AS c_1 FROM pm1.g1 AS g_0 ORDER BY c_0", Arrays.asList("a", false));
+        dataManager.addData("SELECT DISTINCT g_0.e1 AS c_0, g_0.e2 AS c_1, g_0.e3 AS c_2, g_0.e4 AS c_3 FROM pm1.g2 AS g_0 ORDER BY c_0", Arrays.asList("a", 1, false, 1.0));
+        //dataManager.addData("SELECT v_0.c_0, v_0.c_1, v_0.c_2, MAX(v_0.c_3) AS c_3 FROM (SELECT g_0.e2 AS c_0, convert(g_0.e3, string) AS c_1, g_0.e3 AS c_2, g_0.e4 AS c_3 FROM pm1.g3 AS g_0) AS v_0 WHERE v_0.c_2 = FALSE GROUP BY v_0.c_0, v_0.c_1, v_0.c_2 ORDER BY v_0.c_0, v_0.c_1");
+        dataManager.addData("SELECT v_0.c_0, v_0.c_1, v_0.c_2, MAX(v_0.c_3) AS c_3 FROM (SELECT g_0.e2 AS c_0, convert(g_0.e3, string) AS c_1, g_0.e3 AS c_2, g_0.e4 AS c_3 FROM pm1.g3 AS g_0) AS v_0 GROUP BY v_0.c_0, v_0.c_1, v_0.c_2 ORDER BY c_0, c_1");
+        //dataManager.addData("SELECT g_0.e2, convert(g_0.e3, string), g_0.e3, g_0.e4 FROM pm1.g3 AS g_0 WHERE g_0.e3 = FALSE", Arrays.asList(1, "false", false, 1.0));
+
+        CommandContext cc = createCommandContext();
+        cc.setMetadata(RealMetadataFactory.example1Cached());
+        helpProcess(plan, cc, dataManager, expected);
+    }
     
     @Test public void testMultiJoinCriteria2() throws Exception {
         String sql = "SELECT max(t3.e4), max(t2.e4) as s FROM pm1.g1 as t1, pm1.g2 as t2, pm1.g3 as t3, pm1.g4 as t4 "
