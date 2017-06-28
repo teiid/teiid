337c7ed9fefa04ebd12e7b2e347269a11429f6e3
shawkins <shawkins@redhat.com>
2017-06-28 16:16:18 -0400

    TEIID-4393 correcting test

--- test-integration/common/src/test/java/org/teiid/jdbc/TestMMDatabaseMetaData.java
+++ test-integration/common/src/test/java/org/teiid/jdbc/TestMMDatabaseMetaData.java
@@ -58,7 +58,7 @@ import org.teiid.jdbc.util.ResultSetUtil;
 @SuppressWarnings("nls")
 public class TestMMDatabaseMetaData {
     
-    private static final boolean REPLACE_EXPECTED = true;
+    private static final boolean REPLACE_EXPECTED = false;
     private static final boolean WRITE_ACTUAL_RESULTS_TO_FILE = false;
     private static final boolean PRINT_RESULTSETS_TO_CONSOLE = false;
     
