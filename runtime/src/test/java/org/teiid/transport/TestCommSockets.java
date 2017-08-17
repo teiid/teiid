85509ce6538a6de6bbd97f1edde94a09021e611a
shawkins <shawkins@redhat.com>
2017-08-17 15:25:25 -0400

    TEIID-5021: ignoring the test when appropriate for ANON_CIPHER_SUITE

--- runtime/src/test/java/org/teiid/transport/TestCommSockets.java
+++ runtime/src/test/java/org/teiid/transport/TestCommSockets.java
@@ -28,11 +28,14 @@ import java.io.ByteArrayInputStream;
 import java.io.Serializable;
 import java.io.StringReader;
 import java.net.InetSocketAddress;
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
 
 import org.junit.After;
 import org.junit.Before;
@@ -205,6 +208,13 @@ public class TestCommSockets {
 	}
 
 	@Test public void testAnonSSLConnect() throws Exception {
+	    SSLContext context = SSLContext.getDefault();
+	    String[] ciphers = context.getServerSocketFactory().getSupportedCipherSuites();
+	    if (!Arrays.asList(ciphers).contains(SocketUtil.ANON_CIPHER_SUITE)) {
+	        //Cannot test anon if no cipher suite is available
+            return;
+        }
+	    
 		SSLConfiguration config = new SSLConfiguration();
 		config.setMode(SSLConfiguration.ENABLED);
 		config.setEnabledCipherSuites("x"); //ensure that this cipher suite is not used
