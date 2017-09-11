827f449d971703e7361a1b0d7bab355852a9ee52
shawkins <shawkins@redhat.com>
2017-09-11 19:12:13 -0400

    adding an explicit registry

--- build/kits/docker/build-image.sh
+++ build/kits/docker/build-image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-sudo docker build --rm -t teiid/teiid:${project.version} .
+sudo docker build --rm -t registry.hub.docker.com/teiid/teiid:${project.version} .
 
 echo "Want to push the image to Docker Hub (y/n) ?"
 
 read selection
 
 if [ "$selection" == "y" ]; then
-    sudo docker login
-    sudo docker push teiid/teiid:${project.version}
+    sudo docker login registry.hub.docker.com
+    sudo docker push registry.hub.docker.com/teiid/teiid:${project.version}
 fi
