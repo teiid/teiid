8f140b0dbbf21e39ec3422bd0b0dcd7d7d661d0a
shawkins <shawkins@redhat.com>
2017-09-15 17:45:30 -0400

    TEIID-5065 allowing unicode vdb names to be used

--- client/src/main/java/org/teiid/jdbc/JDBCURL.java
+++ client/src/main/java/org/teiid/jdbc/JDBCURL.java
@@ -195,7 +195,7 @@ public class JDBCURL {
         if(value.indexOf('=') >= 0) {
             throw new IllegalArgumentException();
         }        
-        addNormalizedProperty(key, getValidValue(value), p);
+        addNormalizedProperty(getValidValue(key), getValidValue(value), p);
     }
     
     public String getJDBCURL() {
