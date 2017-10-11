e1679a173ef92b10ebc9b9c53058e57b78f8c23d
shawkins <shawkins@redhat.com>
2017-10-11 12:04:06 -0400

    TEIID-5092 fixing unsynchronized static access to simpledateformat

--- connectors/translator-amazon-s3/src/main/java/org/teiid/translator/amazon/s3/S3ProcedureExecution.java
+++ connectors/translator-amazon-s3/src/main/java/org/teiid/translator/amazon/s3/S3ProcedureExecution.java
@@ -70,7 +70,6 @@ public class S3ProcedureExecution implements ProcedureExecution {
 	private RuntimeMetadata metadata;
 	private ExecutionContext ec;
 	private String endpoint;
-	private static SimpleDateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss zzz");
 	
 	private BinaryWSProcedureExecution execution = null;
 	boolean isText = false;
@@ -485,6 +484,7 @@ public class S3ProcedureExecution implements ProcedureExecution {
 		if (!isList) {
 			result.add(endpoint);
 			try {
+			    SimpleDateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss zzz");
 				result.add(lastModified == null?null:new Timestamp(df.parse(lastModified).getTime()));
 			} catch (ParseException e) {
 				result.add(null);
