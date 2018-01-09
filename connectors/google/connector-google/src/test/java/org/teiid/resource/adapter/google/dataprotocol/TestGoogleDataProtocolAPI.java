e42eff437549f45b1edb02222dbf5c1a5f0dcaf6
shawkins <shawkins@redhat.com>
2018-01-09 15:24:02 -0500

    TEIID-5203 filtering columns with no label from the update logic

--- connectors/google/connector-google/src/test/java/org/teiid/resource/adapter/google/dataprotocol/TestGoogleDataProtocolAPI.java
+++ connectors/google/connector-google/src/test/java/org/teiid/resource/adapter/google/dataprotocol/TestGoogleDataProtocolAPI.java
@@ -30,7 +30,13 @@ import java.sql.Timestamp;
 import java.util.Arrays;
 
 import org.junit.Test;
+import org.teiid.resource.adapter.google.gdata.GDataClientLoginAPI;
+import org.teiid.translator.google.api.UpdateSet;
+import org.teiid.translator.google.api.metadata.Column;
 import org.teiid.translator.google.api.metadata.SpreadsheetColumnType;
+
+import com.google.gdata.data.spreadsheet.ListEntry;
+import com.google.gdata.data.spreadsheet.ListFeed;
 
 @SuppressWarnings("nls")
 public class TestGoogleDataProtocolAPI {
@@ -42,6 +48,27 @@ public class TestGoogleDataProtocolAPI {
 		assertEquals("2001-01-01 01:02:03.0", ts.toString());
 		Time t = (Time)GoogleDataProtocolAPI.convertValue(null, Arrays.asList(1.0, 2.0, 3.0, 4.0), SpreadsheetColumnType.TIMEOFDAY);
 		assertEquals("01:02:03", t.toString());
+	}
+	
+	@Test public void testColumnsWithoutLabel() {
+	    GDataClientLoginAPI api = new GDataClientLoginAPI() {
+	        protected com.google.gdata.data.BaseFeed<?,?> getSpreadsheetFeedQuery(com.google.gdata.client.spreadsheet.SpreadsheetQuery squery, java.lang.Class<? extends com.google.gdata.data.BaseFeed<?,?>> feedClass) {
+	            ListFeed lf = new ListFeed();
+	            lf.setEntries(Arrays.asList(new ListEntry()));
+	            return lf;
+	        };
+	    };
+	    Column c1 = new Column();
+	    c1.setLabel("valid");
+	    c1.setDataType(SpreadsheetColumnType.STRING);
+	    c1.setAlphaName("A");
+	    
+	    Column c2 = new Column();
+        c2.setDataType(SpreadsheetColumnType.STRING);
+        c2.setAlphaName("B");
+	    
+        //should succeed without an NPE
+	    api.listFeedUpdate("x", "y", "", Arrays.asList(new UpdateSet("valid", "value")), Arrays.asList(c1, c2));
 	}
 	
 	
