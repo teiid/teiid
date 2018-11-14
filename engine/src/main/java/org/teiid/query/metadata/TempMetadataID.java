147dc665ca7579bd54c1610517cfa952790ea9c4
shawkins <shawkins@redhat.com>
2018-11-13 22:50:11 -0500

    TEIID-5532
    narrowing the commands used to determine usage
    correcting the marking of columns as accessible to be localized

--- engine/src/main/java/org/teiid/query/metadata/TempMetadataID.java
+++ engine/src/main/java/org/teiid/query/metadata/TempMetadataID.java
@@ -141,7 +141,6 @@ public class TempMetadataID implements Serializable, Modifiable, DataModifiable 
     private boolean autoIncrement;
     private boolean notNull;
     private boolean updatable;
-    private boolean accessed;
     
     /**
      * Constructor for group form of metadata ID.
@@ -446,14 +445,6 @@ public class TempMetadataID implements Serializable, Modifiable, DataModifiable 
 			this.name = Symbol.getShortName(this.ID);
 		}
 		return this.name;
-	}
-	
-	public void setAccessed(boolean accessed) {
-		this.accessed = accessed;
-	}
-	
-	public boolean isAccessed() {
-		return accessed;
 	}
 	
 }
