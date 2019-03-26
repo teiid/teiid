328125c56a5c77f689ed046ec49714a8474383fd
shawkins <shawkins@redhat.com>
2019-03-26 11:11:15 -0400

    TEIID-5648: More consistency in how metadata is hidden or not (providing an option to enable metadata visibility)

--- engine/src/main/java/org/teiid/dqp/internal/process/DefaultAuthorizationValidator.java
+++ engine/src/main/java/org/teiid/dqp/internal/process/DefaultAuthorizationValidator.java
@@ -61,9 +61,15 @@ public class DefaultAuthorizationValidator implements AuthorizationValidator {
 	public static final String IGNORE_UNAUTHORIZED_ASTERISK = "ignore_unauthorized_asterisk"; //$NON-NLS-1$
 	private PolicyDecider policyDecider;
 	private boolean ignoreUnauthorizedAsteriskDefault = PropertiesUtils.getBooleanProperty(System.getProperties(), "org.teiid.ignoreUnauthorizedAsterisk", false); //$NON-NLS-1$
+    private boolean metadataRequiresPermission = PropertiesUtils.getBooleanProperty(System.getProperties(), "org.teiid.metadataRequiresPermission", true); //$NON-NLS-1$
 	
 	public DefaultAuthorizationValidator() {
 	}
+	
+	public void setMetadataRequiresPermission(
+            boolean metadataRequiresPermission) {
+        this.metadataRequiresPermission = metadataRequiresPermission;
+    }
 
 	@Override
 	public boolean validate(String[] originalSql, Command command,
@@ -146,6 +152,9 @@ public class DefaultAuthorizationValidator implements AuthorizationValidator {
 		if (policyDecider == null || !policyDecider.validateCommand(commandContext)) {
 			return true;
 		}
+		if (!metadataRequiresPermission) {
+		    return true;
+		}
 		AbstractMetadataRecord parent = record;
 		while (parent.getParent() != null) {
 			parent = parent.getParent();
@@ -161,42 +170,46 @@ public class DefaultAuthorizationValidator implements AuthorizationValidator {
 		if (record instanceof FunctionMethod || record instanceof Procedure) {
 			action = PermissionType.EXECUTE;
 		}
+		HashSet<String> resources = new HashSet<String>(2);
 		
-		//cache permission check
-		Boolean result = isAccessible(record, commandContext, action);
-		if (!result && record instanceof Schema) {
+		boolean result = false;
+		if (record instanceof Schema) {
+		    Boolean cachedResult = commandContext.isAccessible(record);
+	        if (cachedResult != null) {
+	            return cachedResult;
+	        }
 		    Schema s = (Schema)record;
 	        //check if anything can be seen below the schema level
-		    
-		    if (anyAccessible(s.getTables().values(), commandContext, PermissionType.READ)) {
+		    if (anyAccessible(s.getTables().values(), commandContext, resources, PermissionType.READ)) {
 		        result = true;
-		    } else if (anyAccessible(s.getProcedures().values(), commandContext, PermissionType.EXECUTE)) {
+		    } else if (anyAccessible(s.getProcedures().values(), commandContext, resources, PermissionType.EXECUTE)) {
                 result = true;
-            } else if (anyAccessible(s.getFunctions().values(), commandContext, PermissionType.EXECUTE)) {
+            } else if (anyAccessible(s.getFunctions().values(), commandContext, resources, PermissionType.EXECUTE)) {
                 result = true;
             }
+		} else {
+		    result = isAccessibleInternal(record, commandContext, resources, action);
 		}
 		commandContext.setAccessible(record, result);
 		return result;
 	}
 	
-	private boolean anyAccessible(Collection<? extends AbstractMetadataRecord> records, CommandContext commandContext, PermissionType permission) {
+	private boolean anyAccessible(Collection<? extends AbstractMetadataRecord> records, CommandContext commandContext, HashSet<String> resources, PermissionType permission) {
         for (AbstractMetadataRecord record : records) {
-            if (isAccessible(record, commandContext, permission)) {
+            if (isAccessibleInternal(record, commandContext, resources, permission)) {
                 return true;
             }
         }
         return false;
 	}
 
-    private Boolean isAccessible(AbstractMetadataRecord record,
-            CommandContext commandContext, PermissionType action) {
+	private boolean isAccessibleInternal(AbstractMetadataRecord record,
+            CommandContext commandContext, HashSet<String> resources, PermissionType action) {
         Boolean result = commandContext.isAccessible(record);
 		if (result != null) {
 			return result;
 		}
-		
-		HashSet<String> resources = new HashSet<String>(2);
+		resources.clear();
         resources.add(record.getFullName());
 		return this.policyDecider.getInaccessibleResources(action, resources, Context.METADATA, commandContext).isEmpty();
     }
--- test-integration/common/src/test/java/org/teiid/jdbc/TestDataRoles.java
+++ test-integration/common/src/test/java/org/teiid/jdbc/TestDataRoles.java
@@ -25,6 +25,7 @@ package org.teiid.jdbc;
 import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -34,9 +35,14 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.teiid.deployers.VDBRepository;
+import org.teiid.deployers.VirtualDatabaseException;
+import org.teiid.dqp.internal.datamgr.ConnectorManagerRepository.ConnectorManagerException;
+import org.teiid.dqp.internal.process.DataRolePolicyDecider;
+import org.teiid.dqp.internal.process.DefaultAuthorizationValidator;
 import org.teiid.runtime.EmbeddedConfiguration;
 import org.teiid.runtime.EmbeddedServer;
 import org.teiid.runtime.MaterializationManager;
+import org.teiid.translator.TranslatorException;
 
 @SuppressWarnings("nls")
 public class TestDataRoles {
@@ -78,6 +84,13 @@ public class TestDataRoles {
     	}
     	es.getMaterializationManager().executeQuery(es.getVDBRepository().getLiveVDB("role-1"), "select * from vw");
 	}
+
+    private void setAuthorizationValidator(EmbeddedConfiguration ec, boolean metadataRequiresPermission) {
+        DefaultAuthorizationValidator authorizationValidator = new DefaultAuthorizationValidator();
+		authorizationValidator.setMetadataRequiresPermission(metadataRequiresPermission);
+		authorizationValidator.setPolicyDecider(new DataRolePolicyDecider());
+        ec.setAuthorizationValidator(authorizationValidator);
+    }
 	
 	@Test public void testExecuteImmediate() throws Exception {
 		es = new ExtendedEmbeddedServer();
@@ -122,32 +135,17 @@ public class TestDataRoles {
 		es = new ExtendedEmbeddedServer();
 		EmbeddedConfiguration ec = new EmbeddedConfiguration();
 		es.start(ec);
-		es.deployVDB(new ByteArrayInputStream(new String("<vdb name=\"role-1\" version=\"1\">"
-				+ "<model name=\"s1\" type=\"virtual\">"
-				+ "<metadata type = \"DDL\"><![CDATA["
-				+ "CREATE VIEW t2 (col string primary key) options (x 'y') as select 'a' as col;\n"
-				+ "CREATE VIEW t1 (col string, col_hidden string, primary key (col, col_hidden), foreign key (col) references t2) options (x 'y1') as select col, 'b' as col_hidden from t2;\n"
-				+ "CREATE virtual procedure proc1 (param1 string) returns table (proc_col string) as begin end;\n"
-				+ "]]></metadata></model>"
-				+ "<model name=\"s2\" type=\"virtual\">"
-				+ "<metadata type = \"DDL\"><![CDATA["
-				+ "CREATE VIEW t3 as select 'a' as col, 'b' as col_hidden;\n"
-				+ "CREATE virtual procedure proc2 (param2 string) returns table (proc_col string) as begin end;\n"
-				+ "]]></metadata></model>"
-				+ "<data-role name=\"y\" any-authenticated=\"true\">"
-				+ "<permission><resource-name>s1</resource-name><allow-read>true</allow-read></permission>"
-				+ "<permission><resource-name>s1.t1.col_hidden</resource-name><allow-read>false</allow-read></permission>"
-				+ "<permission><resource-name>s1.t2</resource-name><allow-read>false</allow-read></permission>"
-				+ "<permission><resource-name>sysadmin</resource-name><allow-read>true</allow-read></permission>"
-				+ "</data-role></vdb>").getBytes()));
+		deploySampleVDB();
 		
     	Connection c = es.getDriver().connect("jdbc:teiid:role-1", null);
     	Statement s = c.createStatement();
     	
-    	ResultSet rs = s.executeQuery("select * from sys.tables where name like 't_'");
-    	//only t1 should be visible
+    	ResultSet rs = s.executeQuery("select * from sys.tables where name like 't_' order by name");
+    	//only t1/t3 should be visible - both have at least 1 permission
     	assertTrue(rs.next());
     	assertEquals("t1", rs.getString("name"));
+    	rs.next();
+    	assertEquals("t3", rs.getString("name"));
     	assertFalse(rs.next());
     	
     	rs = s.executeQuery("select * from sys.schemas where name like 's_' order by name");
@@ -160,6 +158,7 @@ public class TestDataRoles {
     	//only col should be visible
     	assertTrue(rs.next());
     	assertEquals("col", rs.getString("name"));
+        assertTrue(rs.next());
     	assertFalse(rs.next());
     	
     	rs = s.executeQuery("select * from sys.procedures where name like 'proc_'");
@@ -193,5 +192,72 @@ public class TestDataRoles {
     	//nothing should be visible
     	assertFalse(rs.next());
 	}
+	
+    @Test public void testMetadataWithoutPermission() throws Exception {
+        es = new ExtendedEmbeddedServer();
+        EmbeddedConfiguration ec = new EmbeddedConfiguration();
+        setAuthorizationValidator(ec, false);
+        es.start(ec);
+        deploySampleVDB();
+        
+        Connection c = es.getDriver().connect("jdbc:teiid:role-1", null);
+        Statement s = c.createStatement();
+        
+        ResultSet rs = s.executeQuery("select count(*) from sys.tables where name like 't_'");
+        assertTrue(rs.next());
+        assertEquals(4, rs.getInt(1));
+        
+        rs = s.executeQuery("select count(*) from sys.schemas where name like 's_'");
+        assertTrue(rs.next());
+        assertEquals(2, rs.getInt(1));
+        
+        rs = s.executeQuery("select count(*) from sys.columns where tablename like 't_'");
+        assertTrue(rs.next());
+        assertEquals(6, rs.getInt(1));
+        
+        rs = s.executeQuery("select count(*) from sys.procedures where name like 'proc_'");
+        assertTrue(rs.next());
+        assertEquals(2, rs.getInt(1));
+        
+        rs = s.executeQuery("select count(*) from sysadmin.usage where schemaname like 's_'");
+        assertTrue(rs.next());
+        assertEquals(3, rs.getInt(1));
+        
+        rs = s.executeQuery("select count(*) from sys.properties where name = 'x'");
+        assertTrue(rs.next());
+        assertEquals(2, rs.getInt(1));
+        
+        rs = s.executeQuery("select count(*) from sys.keycolumns where tablename = 't1'");
+        assertTrue(rs.next());
+        assertEquals(3, rs.getInt(1));
+        
+        rs = s.executeQuery("select count(*) from sys.keys where tablename = 't1'");
+        assertTrue(rs.next());
+        assertEquals(2, rs.getInt(1));
+    }
+
+    private void deploySampleVDB() throws VirtualDatabaseException,
+            ConnectorManagerException, TranslatorException, IOException {
+        es.deployVDB(new ByteArrayInputStream(new String("<vdb name=\"role-1\" version=\"1\">"
+                + "<model name=\"s1\" type=\"virtual\">"
+                + "<metadata type = \"DDL\"><![CDATA["
+                + "CREATE VIEW t3 (col string primary key) as select '3' as col;\n"
+                + "CREATE VIEW t2 (col string primary key) options (x 'y') as select 'a' as col;\n"
+                + "CREATE VIEW t1 (col string, col_hidden string, primary key (col, col_hidden), foreign key (col) references t2) options (x 'y1') as select col, 'b' as col_hidden from t2;\n"
+                + "CREATE virtual procedure proc1 (param1 string) returns table (proc_col string) as begin end;\n"
+                + "]]></metadata></model>"
+                + "<model name=\"s2\" type=\"virtual\">"
+                + "<metadata type = \"DDL\"><![CDATA["
+                + "CREATE VIEW t3 as select 'a' as col, 'b' as col_hidden;\n"
+                + "CREATE virtual procedure proc2 (param2 string) returns table (proc_col string) as begin end;\n"
+                + "]]></metadata></model>"
+                + "<data-role name=\"y\" any-authenticated=\"true\">"
+                + "<permission><resource-name>s1</resource-name><allow-read>true</allow-read></permission>"
+                + "<permission><resource-name>s1</resource-name><allow-create>true</allow-create></permission>"
+                + "<permission><resource-name>s1.t1.col_hidden</resource-name><allow-read>false</allow-read></permission>"
+                + "<permission><resource-name>s1.t2</resource-name><allow-read>false</allow-read></permission>"
+                + "<permission><resource-name>sysadmin</resource-name><allow-read>true</allow-read></permission>"
+                + "</data-role></vdb>").getBytes()));
+    }
 	
 }
