79270968f601d837f2a9000112c7a725d3d84626
Steven Hawkins <shawkins@redhat.com>
2019-06-18 14:51:54 -0400

    removing test db framework

--- wildfly/test-integration/db/src/main/resources/ddl/oracle/create_tables.sql
+++
@@ -1,6 +0,0 @@
-
--- these tables are used by custom junit test for testing transactions
-
-create Table g1 (e1 number(5) PRIMARY KEY,   e2 varchar2(50));
-create Table g2 (e1 number(5) REFERENCES g1, e2 varchar2(50));
-   
