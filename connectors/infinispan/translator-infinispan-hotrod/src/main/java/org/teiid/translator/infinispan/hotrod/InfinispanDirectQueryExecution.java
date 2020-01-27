bcab9319e6ce5274f9d4945601dd9453a96d1618
shawkins <shawkins@redhat.com>
2020-01-27 16:16:47 -0500

    TEIID-5881 removing the cache aliasing

--- connectors/infinispan/translator-infinispan-hotrod/src/main/java/org/teiid/translator/infinispan/hotrod/InfinispanDirectQueryExecution.java
+++ connectors/infinispan/translator-infinispan-hotrod/src/main/java/org/teiid/translator/infinispan/hotrod/InfinispanDirectQueryExecution.java
@@ -42,7 +42,6 @@ import org.teiid.translator.TranslatorException;
 public class InfinispanDirectQueryExecution implements ProcedureExecution {
 
     private static Pattern truncatePattern = Pattern.compile("truncate (\\S+)");
-    private static Pattern renamePattern = Pattern.compile("rename (\\S+)\\s+(\\S+)");
 
     protected int columnCount;
     private List<Argument> arguments;
@@ -62,31 +61,11 @@ public class InfinispanDirectQueryExecution implements ProcedureExecution {
     @Override
     public void execute() throws TranslatorException {
         String command = (String) this.arguments.get(0).getArgumentValue().getValue();
-        BasicCache<String, String> aliasCache = getAliasCache(this.connection);
 
         Matcher m = truncatePattern.matcher(command);
         if (m.matches()) {
             String tableName =  m.group(1);
-            clearContents(aliasCache, tableName);
-            return;
-        }
-
-        m = renamePattern.matcher(command);
-        if (m.matches()) {
-            String tableOne = m.group(1);
-            String tableTwo = m.group(2);
-
-            String aliasName = getAliasName(context, aliasCache, tableOne);
-            if (aliasName.equals(tableOne)) {
-                aliasCache.put(fqn(context, tableTwo), tableOne);
-                aliasCache.put(fqn(context, tableOne), tableTwo);
-            } else if (aliasName.equals(tableTwo)) {
-                aliasCache.put(fqn(context, tableOne), tableOne);
-                aliasCache.put(fqn(context, tableTwo), tableTwo);
-            } else {
-                throw new TranslatorException(InfinispanPlugin.Event.TEIID25015,
-                        InfinispanPlugin.Util.gs(InfinispanPlugin.Event.TEIID25015, tableOne, aliasName));
-            }
+            clearContents(tableName);
             return;
         }
 
@@ -94,8 +73,7 @@ public class InfinispanDirectQueryExecution implements ProcedureExecution {
                 InfinispanPlugin.Util.gs(InfinispanPlugin.Event.TEIID25016, command));
     }
 
-    private void clearContents(BasicCache<String, String> aliasCache, String tableName) throws TranslatorException {
-        tableName = getAliasName(context, aliasCache, tableName);
+    private void clearContents(String tableName) throws TranslatorException {
         Table table = metadata.getTable(tableName);
         String cacheName = ProtobufMetadataProcessor.getCacheName(table);
         BasicCache<Object, Object> cache = connection.getCache(cacheName);
@@ -106,40 +84,8 @@ public class InfinispanDirectQueryExecution implements ProcedureExecution {
         cache.clear();
     }
 
-    static String getAliasName(ExecutionContext context, BasicCache<String, String> aliasCache, String alias)
-            throws TranslatorException {
-        String key = fqn(context, alias);
-        String value = aliasCache.get(key);
-        if (value != null) {
-            return value;
-        }
-        return alias;
-    }
-
-    static Table getAliasTable(ExecutionContext context, RuntimeMetadata metadata,
-            BasicCache<String, String> aliasCache, Table table) throws TranslatorException {
-        String alias = table.getFullName();
-        String key = fqn(context, alias);
-        String value = aliasCache.get(key);
-        if (value != null) {
-            alias = value.substring(value.lastIndexOf('.')+1);
-        } else {
-            alias = alias.substring(alias.lastIndexOf('.')+1);
-        }
-        return metadata.getTable(table.getParent().getName(), alias);
-    }
-
     static String fqn(ExecutionContext context, String key) {
         return context.getVdbName()+"."+context.getVdbVersion()+"."+key;
-    }
-
-    static BasicCache<String, String> getAliasCache(InfinispanConnection connection) throws TranslatorException {
-        BasicCache<String, String> cache = connection.getCache(InfinispanExecutionFactory.TEIID_ALIAS_NAMING_CACHE);
-        if (cache == null) {
-            throw new TranslatorException(InfinispanPlugin.Event.TEIID25014, InfinispanPlugin.Util
-                    .gs(InfinispanPlugin.Event.TEIID25014, InfinispanExecutionFactory.TEIID_ALIAS_NAMING_CACHE));
-        }
-        return cache;
     }
 
     @Override
