682561e4f0b9109528fe3fdd1d055dddfa4164d0
Don Krapohl <donald-krapohl-2ef65df578cd44b4ff22380a310f0543@migrations.noreply.github.rakops.com>
2021-06-21 10:13:12 -0400

    again removing CRLF

--- connectors/jdbc/translator-jdbc/src/main/java/org/teiid/translator/jdbc/mm/MetaMatrixExecutionFactory.java
+++ connectors/jdbc/translator-jdbc/src/main/java/org/teiid/translator/jdbc/mm/MetaMatrixExecutionFactory.java
@@ -1,150 +1,1 @@
-/*
- * Copyright © 2000-2005 MetaMatrix, Inc.  All rights reserved.
- */
-/*
- */
-package org.teiid.translator.jdbc.mm;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.teiid.translator.Translator;
-import org.teiid.translator.TranslatorException;
-import org.teiid.translator.TypeFacility;
-import org.teiid.translator.jdbc.JDBCExecutionFactory;
-
-/**
- * Capabilities compatible with MM 5.5.x
- */
-@Translator(name="metamatrix", description="A translator for MetaMatrix 5.5 or later")
-public class MetaMatrixExecutionFactory extends JDBCExecutionFactory {
-
-    @Override
-    public void start() throws TranslatorException {
-        super.start();
-        addPushDownFunction("mm", "timestampdiff", TypeFacility.RUNTIME_NAMES.INTEGER, TypeFacility.RUNTIME_NAMES.STRING, TypeFacility.RUNTIME_NAMES.TIMESTAMP, TypeFacility.RUNTIME_NAMES.TIMESTAMP); //$NON-NLS-1$ //$NON-NLS-2$
-    }
-
-    public List<String> getSupportedFunctions() {
-        List<String> supportedFunctions = new ArrayList<String>();
-        supportedFunctions.addAll(super.getSupportedFunctions());
-        supportedFunctions.add("ABS"); //$NON-NLS-1$
-        supportedFunctions.add("ACOS"); //$NON-NLS-1$
-        supportedFunctions.add("ASIN"); //$NON-NLS-1$
-        supportedFunctions.add("ATAN"); //$NON-NLS-1$
-        supportedFunctions.add("ATAN2"); //$NON-NLS-1$
-        supportedFunctions.add("CEILING"); //$NON-NLS-1$
-        supportedFunctions.add("COS"); //$NON-NLS-1$
-        supportedFunctions.add("COT"); //$NON-NLS-1$
-        supportedFunctions.add("DEGREES"); //$NON-NLS-1$
-        supportedFunctions.add("EXP"); //$NON-NLS-1$
-        supportedFunctions.add("FLOOR"); //$NON-NLS-1$
-        supportedFunctions.add("FORMATBIGDECIMAL"); //$NON-NLS-1$
-        supportedFunctions.add("FORMATBIGINTEGER"); //$NON-NLS-1$
-        supportedFunctions.add("FORMATDOUBLE"); //$NON-NLS-1$
-        supportedFunctions.add("FORMATFLOAT"); //$NON-NLS-1$
-        supportedFunctions.add("FORMATINTEGER"); //$NON-NLS-1$
-        supportedFunctions.add("FORMATLONG"); //$NON-NLS-1$
-        supportedFunctions.add("LOG"); //$NON-NLS-1$
-        supportedFunctions.add("LOG10"); //$NON-NLS-1$
-        supportedFunctions.add("MOD"); //$NON-NLS-1$
-        supportedFunctions.add("PARSEBIGDECIMAL"); //$NON-NLS-1$
-        supportedFunctions.add("PARSEBIGINTEGER"); //$NON-NLS-1$
-        supportedFunctions.add("PARSEDOUBLE"); //$NON-NLS-1$
-        supportedFunctions.add("PARSEFLOAT"); //$NON-NLS-1$
-        supportedFunctions.add("PARSEINTEGER"); //$NON-NLS-1$
-        supportedFunctions.add("PARSELONG"); //$NON-NLS-1$
-        supportedFunctions.add("PI"); //$NON-NLS-1$
-        supportedFunctions.add("POWER"); //$NON-NLS-1$
-        supportedFunctions.add("RADIANS"); //$NON-NLS-1$
-        supportedFunctions.add("RAND"); //$NON-NLS-1$
-        supportedFunctions.add("ROUND"); //$NON-NLS-1$
-        supportedFunctions.add("SIGN"); //$NON-NLS-1$
-        supportedFunctions.add("SIN"); //$NON-NLS-1$
-        supportedFunctions.add("SQRT"); //$NON-NLS-1$
-        supportedFunctions.add("TAN"); //$NON-NLS-1$
-        supportedFunctions.add("ASCII"); //$NON-NLS-1$
-        supportedFunctions.add("CHAR"); //$NON-NLS-1$
-        supportedFunctions.add("CHR"); //$NON-NLS-1$
-        supportedFunctions.add("CONCAT"); //$NON-NLS-1$
-        supportedFunctions.add("CONCAT2"); //$NON-NLS-1$
-        supportedFunctions.add("||"); //$NON-NLS-1$
-        supportedFunctions.add("INITCAP"); //$NON-NLS-1$
-        supportedFunctions.add("INSERT"); //$NON-NLS-1$
-        supportedFunctions.add("LCASE"); //$NON-NLS-1$
-        supportedFunctions.add("LENGTH"); //$NON-NLS-1$
-        supportedFunctions.add("LEFT"); //$NON-NLS-1$
-        supportedFunctions.add("LOCATE"); //$NON-NLS-1$
-        supportedFunctions.add("LPAD"); //$NON-NLS-1$
-        supportedFunctions.add("LTRIM"); //$NON-NLS-1$
-        supportedFunctions.add("REPEAT"); //$NON-NLS-1$
-        supportedFunctions.add("REPLACE"); //$NON-NLS-1$
-        supportedFunctions.add("RPAD"); //$NON-NLS-1$
-        supportedFunctions.add("RIGHT"); //$NON-NLS-1$
-        supportedFunctions.add("RTRIM"); //$NON-NLS-1$
-        supportedFunctions.add("SUBSTRING"); //$NON-NLS-1$
-        supportedFunctions.add("TRANSLATE"); //$NON-NLS-1$
-        supportedFunctions.add("UCASE"); //$NON-NLS-1$
-        supportedFunctions.add("CURDATE"); //$NON-NLS-1$
-        supportedFunctions.add("CURTIME"); //$NON-NLS-1$
-        supportedFunctions.add("NOW"); //$NON-NLS-1$
-        supportedFunctions.add("DAYNAME"); //$NON-NLS-1$
-        supportedFunctions.add("DAYOFMONTH"); //$NON-NLS-1$
-        supportedFunctions.add("DAYOFWEEK"); //$NON-NLS-1$
-        supportedFunctions.add("DAYOFYEAR"); //$NON-NLS-1$
-        supportedFunctions.add("FORMATDATE"); //$NON-NLS-1$
-        supportedFunctions.add("FORMATTIME"); //$NON-NLS-1$
-        supportedFunctions.add("FORMATTIMESTAMP"); //$NON-NLS-1$
-        supportedFunctions.add("HOUR"); //$NON-NLS-1$
-        supportedFunctions.add("MINUTE"); //$NON-NLS-1$
-        supportedFunctions.add("MONTH"); //$NON-NLS-1$
-        supportedFunctions.add("MONTHNAME"); //$NON-NLS-1$
-        supportedFunctions.add("PARSEDATE"); //$NON-NLS-1$
-        supportedFunctions.add("PARSETIME"); //$NON-NLS-1$
-        supportedFunctions.add("PARSETIMESTAMP"); //$NON-NLS-1$
-        supportedFunctions.add("SECOND"); //$NON-NLS-1$
-        supportedFunctions.add("TIMESTAMPADD"); //$NON-NLS-1$
-        //supportedFunctions.add("TIMESTAMPDIFF"); //$NON-NLS-1$
-        supportedFunctions.add("WEEK"); //$NON-NLS-1$
-        supportedFunctions.add("YEAR"); //$NON-NLS-1$
-        supportedFunctions.add("MODIFYTIMEZONE"); //$NON-NLS-1$
-        supportedFunctions.add("DECODESTRING"); //$NON-NLS-1$
-        supportedFunctions.add("DECODEINTEGER"); //$NON-NLS-1$
-        supportedFunctions.add("IFNULL"); //$NON-NLS-1$
-        supportedFunctions.add("NVL");      //$NON-NLS-1$
-        supportedFunctions.add("CAST"); //$NON-NLS-1$
-        supportedFunctions.add("CONVERT"); //$NON-NLS-1$
-        supportedFunctions.add("USER"); //$NON-NLS-1$
-        return supportedFunctions;
-    }
-
-    @Override
-    public boolean supportsInlineViews() {
-        return true;
-    }
-
-    @Override
-    public boolean supportsFunctionsInGroupBy() {
-        return true;
-    }
-
-    @Override
-    public boolean supportsRowLimit() {
-        return true;
-    }
-
-    @Override
-    public boolean supportsRowOffset() {
-        return true;
-    }
-
-    @Override
-    public NullOrder getDefaultNullOrder() {
-        return NullOrder.UNKNOWN;
-    }
-
-    @Override
-    public boolean supportsSelectWithoutFrom() {
-        return true;
-    }
-}
+/* * Copyright © 2000-2005 MetaMatrix, Inc.  All rights reserved. *//* */package org.teiid.translator.jdbc.mm;import java.util.ArrayList;import java.util.List;import org.teiid.translator.Translator;import org.teiid.translator.TranslatorException;import org.teiid.translator.TypeFacility;import org.teiid.translator.jdbc.JDBCExecutionFactory;/** * Capabilities compatible with MM 5.5.x */@Translator(name="metamatrix", description="A translator for MetaMatrix 5.5 or later")public class MetaMatrixExecutionFactory extends JDBCExecutionFactory {    @Override    public void start() throws TranslatorException {        super.start();        addPushDownFunction("mm", "timestampdiff", TypeFacility.RUNTIME_NAMES.INTEGER, TypeFacility.RUNTIME_NAMES.STRING, TypeFacility.RUNTIME_NAMES.TIMESTAMP, TypeFacility.RUNTIME_NAMES.TIMESTAMP); //$NON-NLS-1$ //$NON-NLS-2$    }    public List<String> getSupportedFunctions() {        List<String> supportedFunctions = new ArrayList<String>();        supportedFunctions.addAll(super.getSupportedFunctions());        supportedFunctions.add("ABS"); //$NON-NLS-1$        supportedFunctions.add("ACOS"); //$NON-NLS-1$        supportedFunctions.add("ASIN"); //$NON-NLS-1$        supportedFunctions.add("ATAN"); //$NON-NLS-1$        supportedFunctions.add("ATAN2"); //$NON-NLS-1$        supportedFunctions.add("CEILING"); //$NON-NLS-1$        supportedFunctions.add("COS"); //$NON-NLS-1$        supportedFunctions.add("COT"); //$NON-NLS-1$        supportedFunctions.add("DEGREES"); //$NON-NLS-1$        supportedFunctions.add("EXP"); //$NON-NLS-1$        supportedFunctions.add("FLOOR"); //$NON-NLS-1$        supportedFunctions.add("FORMATBIGDECIMAL"); //$NON-NLS-1$        supportedFunctions.add("FORMATBIGINTEGER"); //$NON-NLS-1$        supportedFunctions.add("FORMATDOUBLE"); //$NON-NLS-1$        supportedFunctions.add("FORMATFLOAT"); //$NON-NLS-1$        supportedFunctions.add("FORMATINTEGER"); //$NON-NLS-1$        supportedFunctions.add("FORMATLONG"); //$NON-NLS-1$        supportedFunctions.add("LOG"); //$NON-NLS-1$        supportedFunctions.add("LOG10"); //$NON-NLS-1$        supportedFunctions.add("MOD"); //$NON-NLS-1$        supportedFunctions.add("PARSEBIGDECIMAL"); //$NON-NLS-1$        supportedFunctions.add("PARSEBIGINTEGER"); //$NON-NLS-1$        supportedFunctions.add("PARSEDOUBLE"); //$NON-NLS-1$        supportedFunctions.add("PARSEFLOAT"); //$NON-NLS-1$        supportedFunctions.add("PARSEINTEGER"); //$NON-NLS-1$        supportedFunctions.add("PARSELONG"); //$NON-NLS-1$        supportedFunctions.add("PI"); //$NON-NLS-1$        supportedFunctions.add("POWER"); //$NON-NLS-1$        supportedFunctions.add("RADIANS"); //$NON-NLS-1$        supportedFunctions.add("RAND"); //$NON-NLS-1$        supportedFunctions.add("ROUND"); //$NON-NLS-1$        supportedFunctions.add("SIGN"); //$NON-NLS-1$        supportedFunctions.add("SIN"); //$NON-NLS-1$        supportedFunctions.add("SQRT"); //$NON-NLS-1$        supportedFunctions.add("TAN"); //$NON-NLS-1$        supportedFunctions.add("ASCII"); //$NON-NLS-1$        supportedFunctions.add("CHAR"); //$NON-NLS-1$        supportedFunctions.add("CHR"); //$NON-NLS-1$        supportedFunctions.add("CONCAT"); //$NON-NLS-1$        supportedFunctions.add("CONCAT2"); //$NON-NLS-1$        supportedFunctions.add("||"); //$NON-NLS-1$        supportedFunctions.add("INITCAP"); //$NON-NLS-1$        supportedFunctions.add("INSERT"); //$NON-NLS-1$        supportedFunctions.add("LCASE"); //$NON-NLS-1$        supportedFunctions.add("LENGTH"); //$NON-NLS-1$        supportedFunctions.add("LEFT"); //$NON-NLS-1$        supportedFunctions.add("LOCATE"); //$NON-NLS-1$        supportedFunctions.add("LPAD"); //$NON-NLS-1$        supportedFunctions.add("LTRIM"); //$NON-NLS-1$        supportedFunctions.add("REPEAT"); //$NON-NLS-1$        supportedFunctions.add("REPLACE"); //$NON-NLS-1$        supportedFunctions.add("RPAD"); //$NON-NLS-1$        supportedFunctions.add("RIGHT"); //$NON-NLS-1$        supportedFunctions.add("RTRIM"); //$NON-NLS-1$        supportedFunctions.add("SUBSTRING"); //$NON-NLS-1$        supportedFunctions.add("TRANSLATE"); //$NON-NLS-1$        supportedFunctions.add("UCASE"); //$NON-NLS-1$        supportedFunctions.add("CURDATE"); //$NON-NLS-1$        supportedFunctions.add("CURTIME"); //$NON-NLS-1$        supportedFunctions.add("NOW"); //$NON-NLS-1$        supportedFunctions.add("DAYNAME"); //$NON-NLS-1$        supportedFunctions.add("DAYOFMONTH"); //$NON-NLS-1$        supportedFunctions.add("DAYOFWEEK"); //$NON-NLS-1$        supportedFunctions.add("DAYOFYEAR"); //$NON-NLS-1$        supportedFunctions.add("FORMATDATE"); //$NON-NLS-1$        supportedFunctions.add("FORMATTIME"); //$NON-NLS-1$        supportedFunctions.add("FORMATTIMESTAMP"); //$NON-NLS-1$        supportedFunctions.add("HOUR"); //$NON-NLS-1$        supportedFunctions.add("MINUTE"); //$NON-NLS-1$        supportedFunctions.add("MONTH"); //$NON-NLS-1$        supportedFunctions.add("MONTHNAME"); //$NON-NLS-1$        supportedFunctions.add("PARSEDATE"); //$NON-NLS-1$        supportedFunctions.add("PARSETIME"); //$NON-NLS-1$        supportedFunctions.add("PARSETIMESTAMP"); //$NON-NLS-1$        supportedFunctions.add("SECOND"); //$NON-NLS-1$        supportedFunctions.add("TIMESTAMPADD"); //$NON-NLS-1$        //supportedFunctions.add("TIMESTAMPDIFF"); //$NON-NLS-1$        supportedFunctions.add("WEEK"); //$NON-NLS-1$        supportedFunctions.add("YEAR"); //$NON-NLS-1$        supportedFunctions.add("MODIFYTIMEZONE"); //$NON-NLS-1$        supportedFunctions.add("DECODESTRING"); //$NON-NLS-1$        supportedFunctions.add("DECODEINTEGER"); //$NON-NLS-1$        supportedFunctions.add("IFNULL"); //$NON-NLS-1$        supportedFunctions.add("NVL");      //$NON-NLS-1$        supportedFunctions.add("CAST"); //$NON-NLS-1$        supportedFunctions.add("CONVERT"); //$NON-NLS-1$        supportedFunctions.add("USER"); //$NON-NLS-1$        return supportedFunctions;    }    @Override    public boolean supportsInlineViews() {        return true;    }    @Override    public boolean supportsFunctionsInGroupBy() {        return true;    }    @Override    public boolean supportsRowLimit() {        return true;    }    @Override    public boolean supportsRowOffset() {        return true;    }    @Override    public NullOrder getDefaultNullOrder() {        return NullOrder.UNKNOWN;    }    @Override    public boolean supportsSelectWithoutFrom() {        return true;    }}
